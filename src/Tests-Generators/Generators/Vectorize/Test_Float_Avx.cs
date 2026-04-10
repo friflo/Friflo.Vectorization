2c527038015306da750c4274e93cc10ff65628a7
friflo <ullrich@praetz.de>
2026-04-10 10:26:49 +0200

    add Vector test skeleton

---
+++ src/Tests-Generators/Generators/Vectorize/Test_Float_Avx.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+
+using Friflo.Vectorization;
+using NUnit.Framework;
+
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+namespace Tests.Generators.Vectorize;
+
+
+
+public static partial class Test_Float_Avx
+{
+    // -----------------------------------------------------------------------------------------------------
+    // [Vectorize] [OmitHash]
+    private static void Multiply([Span] ref float position, [Span] float velocity) {
+        position *= velocity;
+    } 
+        
+    [Test]
+    public static void Test_Avx_Multiply()
+    {
+        var position        = new float[128];
+        var positionVector  = new float[128];
+        var velocity        = new float[128];
+        for (int n = 0; n < 128; n++) {
+            position[n] = positionVector[n] = n;
+            velocity[n] = n + 100;
+        }
+        // MultiplyVector(position,        velocity);
+        // MultiplyVector(positionVector,  velocity, false);
+        
+        for (int n = 0; n < 128; n++) {
+            Assert.That(position[n], Is.EqualTo(positionVector[n]));
+        }
+    }
+}
