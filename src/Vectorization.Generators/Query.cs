68657771e0dd351d7ec3415d48e76936b0285730
friflo <ullrich@praetz.de>
2026-04-19 16:27:11 +0200

    update VerticalAoS rules

--- src/Vectorization.Generators/Query.cs
+++ src/Vectorization.Generators/Query.cs
@@ -18,7 +18,7 @@ namespace Friflo.Vectorization.Generators;
 //      Otherwise -> MixedAdapter
 // Strategy
 //  NativeSoA:      Result from Traversal is final. No second pass. "Golden Path"
-//  VerticalAoS:    Escalates to Horizontal always on: Cross, Normalize, Slerp, Reflect
+//  VerticalAoS:    Escalates to Horizontal always on: Transform | Dot,Cross | Distance, Length, LengthSquared | Normalize | Slerp, Reflect
 //               or Escalates to Horizontal whenever an operation’s required input shape deviates from the passed parameter's data shape
 //  MixedAdapter:   Second MixedAdapter pass required to apply Deinterleave() / Interleave()
 //  Horizontal:     Escalated from VerticalAoS. Its result is final.
