a594c9b8bf9940c38a7d1ff1cf99f4e8b78c1393
friflo <ullrich@praetz.de>
2026-05-03 22:42:15 +0200

    add docs

--- src/Tests-Generators/Generators/GPU/Test/Utils/Dbg.cs
+++ src/Tests-Generators/Generators/GPU/Test/Utils/Dbg.cs
@@ -1,6 +1,10 @@
 ﻿
 using Tests.Generators.GPU;
 
+/// <summary>
+/// Used to check GPU handle counts within a debugger from any place using:<br/>
+/// <c>Dbg.HandleDiff</c>. 
+/// </summary>
 internal static class Dbg
 {
     internal static GpuTestBase  Instance;
