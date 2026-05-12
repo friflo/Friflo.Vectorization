27c278e5a89cd93fe9f1fbc0c4c243b6d402b549
friflo <ullrich@praetz.de>
2026-05-12 16:46:33 +0200

    GPU - cleanup

--- src/Tests-Generators/GPU/Test/Utils/GpuTestGlobal.cs
+++ src/Tests-Generators/GPU/Test/Utils/GpuTestGlobal.cs
@@ -1,8 +1,7 @@
 ﻿using System.Linq;
 using Friflo.Vectorization.GPU;
+using Friflo.Vectorization.WebGPU.Runtime;
 using NUnit.Framework;
-using Silk.NET.WebGPU;
-using Silk.NET.WebGPU.Extensions.WGPU;
 
 // ReSharper disable InconsistentNaming
 // ReSharper disable ConvertToConstant.Global
@@ -39,21 +38,21 @@ public sealed class GpuTestGlobal
     }
     
     private static void SetupWebGPU () {
-        var instance = Friflo.Vectorization.WebGPU.WgpuInstance.CreateInstance(new Friflo.Vectorization.WebGPU.Runtime.InstanceExtras {
+        var instance = Friflo.Vectorization.WebGPU.WgpuInstance.CreateInstance(new InstanceExtras {
             // Backends            = InstanceBackend.DX12,
         });
         var infos       = instance.GetAdapterInfos();
-        var adapterInfo = infos.FirstOrDefault(props => props.BackendType == Friflo.Vectorization.WebGPU.Runtime.BackendType.D3D12);
+        var adapterInfo = infos.FirstOrDefault(props => props.BackendType == BackendType.D3D12);
         Adapter         = instance.RequestAdapter(default, null); // adapterInfo <= use specific adapter
         Instance        = instance;
     }
     
     private static void SetupSilk () {
-        var instance = Friflo.Vectorization.SilkWebGPU.WgpuInstance.CreateInstance(new InstanceExtras {
+        var instance = Friflo.Vectorization.SilkWebGPU.WgpuInstance.CreateInstance(new Silk.NET.WebGPU.Extensions.WGPU.InstanceExtras {
             // Backends            = InstanceBackend.DX12,
         });
         var infos       = instance.GetAdapterInfos();
-        var adapterInfo = infos.FirstOrDefault(props => props.BackendType == BackendType.D3D12);
+        var adapterInfo = infos.FirstOrDefault(props => props.BackendType == Silk.NET.WebGPU.BackendType.D3D12);
         Adapter         = instance.RequestAdapter(default, null); // adapterInfo <= use specific adapter
         Instance        = instance;
     }
