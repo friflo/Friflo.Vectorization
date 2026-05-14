1be14e3a4c0d296902ae44de7d039ab7097b817e
friflo <ullrich@praetz.de>
2026-05-14 16:23:39 +0200

    Generator - add WGSL Vectorizer files

---
+++ src/Vectorization.Generators/Vectorizer/WGSL/Entry.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+
+// ReSharper disable once CheckNamespace
+namespace Friflo.Vectorization.Generators.AVX;
+
+public static partial class WgslVectorizer
+{
+}
