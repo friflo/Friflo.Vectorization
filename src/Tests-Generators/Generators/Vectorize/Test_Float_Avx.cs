5a176abe1729c24df67279cfd7c3d168f2b30b3c
friflo <ullrich@praetz.de>
2026-05-17 09:07:30 +0200

    add Avx_Trigonometry2()

--- src/Tests-Generators/Generators/Vectorize/Test_Float_Avx.cs
+++ src/Tests-Generators/Generators/Vectorize/Test_Float_Avx.cs
@@ -2,6 +2,7 @@
 // See LICENSE file in the project root for full license information.
 
 
+using System;
 using Friflo.Vectorization;
 using NUnit.Framework;
 
@@ -35,6 +36,35 @@ public static partial class Test_Float_Avx
         
         for (int n = 0; n < 128; n++) {
             Assert.That(position[n], Is.EqualTo(positionVector[n]));
+        }
+    }
+    
+    // ----------------------------------------------
+    [Vectorize] [OmitHash]
+    private static void Avx_Trigonometry2([Span]ref float position)
+    {
+        var sinh     = MathF.Sinh(position);
+        var cosh     = MathF.Cosh(position);
+        var tanh     = MathF.Tanh(position);
+        position += sinh + cosh + tanh;
+    }
+    
+    [Test]
+    public static void Test_Avx_Trigonometry2()
+    {
+        var scalar1     = new float[128];
+        var scalar2     = new float[128];
+        for (int n = 0; n < 128; n++) {
+            scalar1[n] = scalar2[n] = (n - 64f) / 64f * 10;
+        }
+
+        Avx_Trigonometry2Vector(scalar1, false);
+        Avx_Trigonometry2Vector(scalar2);
+        
+        
+        for (int n = 0; n < 128; n++) {
+            Assert.That(scalar1[n], Is.EqualTo(scalar2[n]).Within(0.01).Percent);
+            Assert.That(scalar1[n], Is.Not.NaN & Is.Not.EqualTo(float.PositiveInfinity) & Is.Not.EqualTo(float.NegativeInfinity));
         }
     }
 }
