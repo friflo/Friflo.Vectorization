d2db77497732bfa79f545b63696a488cdeed31cb
friflo <ullrich@praetz.de>
2026-05-18 09:50:27 +0200

    Generator - implement: CalculateLayout()

--- src/Vectorization.Generators/Generator/UniformCalculator.cs
+++ src/Vectorization.Generators/Generator/UniformCalculator.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 
 // ReSharper disable CheckNamespace
@@ -22,8 +24,43 @@ public class UniformField
 
 public static class UniformCalculator
 {
+    // "Greedy-Packing-Strategy"
+    // Creates optimal struct layout with
+    // - 16-byte alignment (std140/std430)
+    // - correct field alignment
     public static int CalculateLayout(List<UniformField> fields)
     {
-        return 0;
+        if (fields.Count == 0) return 0;
+
+        // 1. Sort by alignment descending to minimize padding gaps naturally
+        // If alignments are equal, sort by size descending
+        var orderedFields = fields
+            .OrderByDescending(f => f.alignment)
+            .ThenByDescending(f => f.size)
+            .ToList();
+
+        int currentOffset = 0;
+        int maxAlignment = 16; // WGSL uniform buffers require minimum 16-byte base alignment
+
+        foreach (var field in orderedFields) {
+            maxAlignment = Math.Max(maxAlignment, field.alignment);
+
+            // 2. Align current offset to the field's required alignment
+            int remainder = currentOffset % field.alignment;
+            if (remainder != 0) {
+                currentOffset += (field.alignment - remainder);
+            }
+            // 3. Assign calculated offset
+            field.offset = currentOffset;
+            // 4. Advance offset by the actual size of the data
+            currentOffset += field.size;
+        }
+        // 5. Pad the final structure size to a multiple of the largest alignment
+        int finalRemainder = currentOffset % maxAlignment;
+        if (finalRemainder != 0) {
+            currentOffset += (maxAlignment - finalRemainder);
+        }
+        // Write back the ordered results to the original list references
+        return currentOffset;
     }
 }
