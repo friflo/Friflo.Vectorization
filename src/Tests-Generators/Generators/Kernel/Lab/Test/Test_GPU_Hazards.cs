70553a4a17c976ba56cc561b822425f4ef84554e
friflo <ullrich@praetz.de>
2026-05-28 16:33:57 +0200

    tests - add class Test_GPU_Hazards

---
+++ src/Tests-Generators/Generators/Kernel/Lab/Test/Test_GPU_Hazards.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Friflo.Vectorization.GPU;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace Kernel.Lab;
+
+
+public class Test_GPU_Hazards : KernelBase
+{
+    [Test]
+    public void Test_GPU_Repeat()
+    {
+        {
+            using var device = Device;
+
+            var weight  = new float[64]; // no alignment
+            var input   = new float[64];
+            var output  = new float[64];
+            for (int n = 0; n < 64; ++n) {
+                weight[n] = n;
+                input[n]  = n + 1000;
+            }
+            using var gpuWeight   = device.CreateBuffer(weight, "gpuWeight", BufferProfile.StaticIn);
+            using var gpuInput    = device.CreateBuffer(input,  "gpuInput",  BufferProfile.StaticIn);
+            using var gpuOutput   = device.CreateBuffer(output, "gpuOutput", BufferProfile.InOut);
+            
+            var context = device.PipelineContext; 
+            context.EnableDiagnostics  = true;
+            context.EnablePassBatching = true;
+            
+            for (int n = 0; n < 5; ++n) {
+                GpuPattern.ShadowMethod(gpuWeight.In, gpuInput.In, 42, gpuOutput.InOut);
+            }
+            device.Download();
+            Assert.AreEqual(3,              context.Records.Length);
+            Assert.AreEqual("ShadowMethod", context.Records[0].KernelName);
+            Assert.AreEqual("""
+                            --- PIPELINE TRACE (Batching: True  Diagnostics: True  Records: 3) ---
+                            // Lock-free GPU kernels with deferred, hazard-driven pass batching
+                            'ShadowMethod'  calls: 5  passes: 1
+                            [KernelSubmit]  'ShadowMethod'
+                            [BatchSubmit]
+                            """, context.RecordLog);
+            
+            context.EnablePassBatching = false;
+            context.ClearRecords();
+
+            GpuPattern.ShadowMethod(gpuWeight.In, gpuInput.In, 42, gpuOutput.InOut);
+            GpuPattern.ShadowMethod(gpuWeight.In, gpuInput.In, 42, gpuOutput.InOut);
+ 
+            device.Download();
+            
+            Assert.AreEqual("""
+                            --- PIPELINE TRACE (Batching: False  Diagnostics: True  Records: 5) ---
+                            'ShadowMethod'  calls: 1  passes: 1
+                            [KernelSubmit]  'ShadowMethod'
+                            'ShadowMethod'  calls: 1  passes: 1
+                            [KernelSubmit]  'ShadowMethod'
+                            [BatchSubmit]
+                            """, context.RecordLog);
+        }
+        Console.WriteLine(HandleDiff.GetState());
+    }
+}
--- src/Tests-Generators/Generators/Kernel/Lab/Test/Test_GPU_Misc.cs
+++ src/Tests-Generators/Generators/Kernel/Lab/Test/Test_GPU_Misc.cs
@@ -1,4 +1,5 @@
-﻿using Friflo.Vectorization.GPU;
+﻿using System;
+using Friflo.Vectorization.GPU;
 using NUnit.Framework;
 
 // ReSharper disable InconsistentNaming
@@ -23,5 +24,30 @@ public class Test_GPU_Misc : KernelBase
         
         Assert.AreEqual("GpuBuffer<float> 'InOut'  Length: 10",     inOutBuffer.ToString());
         Assert.AreEqual("GpuBuffer<float> 'StaticIn'  Length: 10",  staticBuffer.ToString());
+    }
+    
+    [Test]
+    public void Test_GPU_Adapter()
+    {
+        using var gpuWeight   = Device.CreateBuffer<float>(100, "test-buffer", BufferProfile.StaticIn);
+        Assert.AreEqual("test-buffer",  gpuWeight.Label);
+        Assert.AreEqual(100,            gpuWeight.Length);
+        
+        Assert.NotNull(Instance.GetAdapterInfos());
+        
+        var info = Adapter.GetAdapterInfo();
+        Console.WriteLine($"Adapter: {info.AdapterType}  Backend: {info.BackendType}  Name: {info.Name}  Driver: {info.DriverDescription}  VendorID: {info.VendorID}  DeviceID: {info.DeviceID}");
+        
+        Assert.AreEqual("GpuTestBase", Device.Label);
+        
+        var adapterLimits = Adapter.GetAdapterLimits();
+        Console.WriteLine($@"
+MaxStorageBufferBindingSize:        {adapterLimits.MaxStorageBufferBindingSize}
+MaxComputeWorkgroupStorageSize:     {adapterLimits.MaxComputeWorkgroupStorageSize}
+MaxBindGroups:                      {adapterLimits.MaxBindGroups}
+MaxComputeInvocationsPerWorkgroup:  {adapterLimits.MaxComputeInvocationsPerWorkgroup}");
+        
+        _ = Device.GetDeviceLimits();
+        
     }
 }
