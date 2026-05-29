2a80618fd86d6dcd036599486ea556dd734dfb34
friflo <ullrich@praetz.de>
2026-05-29 18:14:11 +0200

    core - hide PipelineRecorder in editor

--- src/Vectorization.GPU/Runtime/PipelineRecorder.cs
+++ src/Vectorization.GPU/Runtime/PipelineRecorder.cs
@@ -2,10 +2,12 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
+using System.ComponentModel;
 
 // ReSharper disable once CheckNamespace
 namespace Friflo.Vectorization.GPU.Runtime;
 
+[EditorBrowsable(EditorBrowsableState.Never)]
 public class PipelineRecorder
 {
     protected internal virtual  bool                        EnablePassBatching  { get; set; }
