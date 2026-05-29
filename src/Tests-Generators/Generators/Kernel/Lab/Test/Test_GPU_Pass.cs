e6ff450b32330231c9e24893e54c886f6026bf23
friflo <ullrich@praetz.de>
2026-05-29 09:49:24 +0200

    tests - Test_GPU_Pass_Batching() add test without traces

--- src/Tests-Generators/Generators/Kernel/Lab/Test/Test_GPU_Pass.cs
+++ src/Tests-Generators/Generators/Kernel/Lab/Test/Test_GPU_Pass.cs
@@ -24,13 +24,14 @@ public class Test_GPU_Pass : KernelBase
         using var gpuOutput   = device.CreateBuffer(output, "gpuOutput", BufferProfile.InOut);
         
         var context = device.PipelineContext; 
-        context.EnableTraces  = true;
-        context.EnablePassBatching = true;
+        context.EnableTraces        = true;
+        context.EnablePassBatching  = true;
         
         for (int n = 0; n < 5; ++n) {
             GpuPattern.ShadowMethod(gpuWeight.In, gpuInput.In, 42, gpuOutput.InOut);
         }
         device.Download();
+        
         Assert.AreEqual(3,              context.Traces.Length);
         Assert.AreEqual("ShadowMethod", context.Traces[0].KernelName);
         Assert.AreEqual("calls: 5  passes: 1  hazards: 0", context.Stats.ToString());
@@ -42,7 +43,25 @@ public class Test_GPU_Pass : KernelBase
                         [Batch_Submit]
                         """, context.TraceLog);
         
-        context.EnablePassBatching = false;
+        // --- same without traces
+        context.EnableTraces  = false;
+        context.ClearTraces();
+        
+        for (int n = 0; n < 5; ++n) {
+            GpuPattern.ShadowMethod(gpuWeight.In, gpuInput.In, 42, gpuOutput.InOut);
+        }
+        device.Download();
+        
+        Assert.AreEqual(0, context.Traces.Length);
+        Assert.AreEqual("calls: 5  passes: 1  hazards: 0", context.Stats.ToString());
+        Assert.AreEqual("""
+                        --- PIPELINE TRACE (Batching: True  Traces: False  Count: 0) ---
+                        --- Lock-free GPU kernels with deferred, hazard-driven pass batching
+                        """, context.TraceLog);
+        
+        // --- Force hazards
+        context.EnablePassBatching  = false;
+        context.EnableTraces        = true;
         context.ClearTraces();
 
         GpuPattern.ShadowMethod(gpuWeight.In, gpuInput.In, 42, gpuOutput.InOut);
@@ -89,6 +108,7 @@ public class Test_GPU_Pass : KernelBase
                         [Batch_Submit]
                         """, context.TraceLog);
         
+        // --- same without traces
         context.EnableTraces  = false;
         context.ClearTraces();
         
