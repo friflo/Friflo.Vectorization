9b874a274ebe8494da46fe758f3ccb46a1765a57
friflo <ullrich@praetz.de>
2026-06-01 20:57:02 +0200

    cleanup

--- src/Vectorization.WebGPU/Runtime/Recorder.cs
+++ src/Vectorization.WebGPU/Runtime/Recorder.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
