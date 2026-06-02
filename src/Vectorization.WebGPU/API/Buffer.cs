c82ee4e2632abf55848fedc0ee551e7b1c01672c
friflo <ullrich@praetz.de>
2026-06-02 11:24:17 +0200

    wgpu - removed obsolete WgpuBuffer<> methods

--- src/Vectorization.WebGPU/API/Buffer.cs
+++ src/Vectorization.WebGPU/API/Buffer.cs
@@ -62,64 +62,6 @@ public sealed unsafe class WgpuBuffer<T> : GpuBuffer<T>, IWgpuBuffer where T : u
         data            = new BufferData(bufferId, Marshal.SizeOf<T>(), Length, buffer, statingHandle);
     }
     
-    // TODO obsolete - kept temporary for reference - will be removed
-#if DEBUG
-    private void Download(GpuBuffer<T> gpuBuffer, T[] targetArray) // TODO  optimize DeviceCreateBuffer und DeviceCreateCommandEncoder are heavy operations
-    {
-        var dev = device;
-        // dev.Flush();  irrelevant code
-        
-        if (targetArray.Length < gpuBuffer.Length)
-            throw new Exception("Target array is too small!");
-
-        uint size = (uint)(gpuBuffer.Length * sizeof(T));
-        
-        var readDesc = new BufferDescriptor {
-            size = size,
-            usage = (ulong)(BufferUsage.CopyDst | BufferUsage.MapRead),
-            mappedAtCreation = WgpuUtils.FromBool(false)
-        };
-        var DevicePtr   = dev.DevicePtr;
-        var QueuePtr    = dev.QueuePtr;
-        var readBuffer  = wgpuDeviceCreateBuffer(DevicePtr, &readDesc);
-
-        var encoder = wgpuDeviceCreateCommandEncoder(DevicePtr, null);
-        wgpuCommandEncoderCopyBufferToBuffer(encoder, ((WgpuBuffer<T>)gpuBuffer).handle, 0, readBuffer, 0, size);
-        
-        var commandBuffer = wgpuCommandEncoderFinish(encoder, null);
-        wgpuQueueSubmit(QueuePtr, 1, &commandBuffer);  	// releases commandBuffer
-        wgpuCommandEncoderRelease(encoder);            	// Not sure if required.
-        wgpuCommandBufferRelease(commandBuffer);       	// Not sure if required. QueueSubmit() seems to release
-
-        // asynchronous mapping
-        bool mapFinished = false;
-        // var callback = PfnBufferMapCallback.From((_, _) => { mapFinished = true; });
-        var callbackInfo = new BufferMapCallbackInfo {
-            mode        = CallbackMode.WaitAnyOnly, // requires blocking wgpuInstanceWaitAny()
-            callback    = &BufferUtils.BufferMap_callback,
-            userdata1   = &mapFinished
-        };
-        var future = wgpuBufferMapAsync(readBuffer, (ulong)MapMode.Read, 0, size, callbackInfo);
-        if (future.id != 0) {
-            var waitInfo = new FutureWaitInfo { future = future, completed = 0 };
-            wgpuInstanceWaitAny(device.instance, 1, &waitInfo, uint.MaxValue);
-        }
-        while (!mapFinished) {   // used in wgpu v19
-            // same as: wgpuDevicePoll(DevicePtr, WgpuUtils.FromBool(true), null);
-            wgpuInstanceProcessEvents(device.instance);
-        }
-        // get result back in original array
-        void* pMapped = wgpuBufferGetMappedRange(readBuffer, 0, size);
-        fixed (void* pTarget = targetArray) {
-            System.Buffer.MemoryCopy(pMapped, pTarget, size, size);
-        }
-        // cleanup
-        wgpuBufferUnmap(readBuffer);
-        wgpuBufferDestroy(readBuffer);
-        wgpuBufferRelease(readBuffer);
-    }
-#endif
-    
     // --- IWgpuBuffer
     BufferData IWgpuBuffer.GetBufferData() => data;
     
@@ -142,17 +84,4 @@ public sealed unsafe class WgpuBuffer<T> : GpuBuffer<T>, IWgpuBuffer where T : u
         }
     }
 }
-
-#if DEBUG
-// TODO obsolete - was used by Download() - will be removed
-internal static class BufferUtils
-{
-    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvCdecl)])]
-    internal static unsafe void BufferMap_callback(MapAsyncStatus status, StringView message, void* userdata1, void* userdata2) {
-        if (userdata1== null) return;
-        var mapFinished = (bool*)userdata1;
-        *mapFinished = true;
-    }
-}
-#endif
 
