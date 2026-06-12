3db1f95d7f161698721425935a99533a311cf6a5
friflo <ullrich@praetz.de>
2026-06-12 07:53:18 +0200

    wgpu - moved kernel methods to Device.Kernel.cs

---
+++ src/Vectorization.WebGPU/API/Device.Kernel.cs
@@ -0,0 +1,173 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Friflo.Vectorization.WebGPU.Runtime;
+using static Friflo.Vectorization.WebGPU.Runtime.WebGPU_native;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable once CheckNamespace
+namespace Friflo.Vectorization.WebGPU;
+
+// -------------- contains methods used by generated kernel methods -------------- 
+public sealed unsafe partial  class WgpuDevice
+{
+    public CommandRecorder Recorder
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] [StackTraceHidden]
+        get {
+            var context = Context;
+            if (context == null) throw MissingContextException();
+            ValidateThreadSafety(context);
+            return (CommandRecorder)context;
+        }
+    }
+    
+    private InvalidOperationException MissingContextException() {
+        return new InvalidOperationException($"Missing Device Context: '{Label}'. Call:  using var context = device.BeginContext();  before calling kernel method.");
+    }
+
+    
+    
+    // --- effectSlots
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ref WgpuEffect GetEffect(int slot, ulong wgslHash)
+    {
+        var slots = effectSlots;
+        if (slot < slots.Length) {
+            ref var effect = ref slots[slot];
+            if (effect.wgslHash == wgslHash) {
+                return ref effect;
+            }
+        }
+        return ref MissingEffect;
+    }
+    
+    private static WgpuEffect MissingEffect;
+    
+    public ref WgpuEffect CreateEffect(
+        int                     kernelId,
+        ulong                   wgslHash,
+        WgpuComputePipeline     pipeline,
+        WgpuBindGroupLayout     bufferLayout,
+        WgpuBindGroupLayout     uniformLayout)
+    {
+        var slots = effectSlots;
+        if (kernelId >= slots.Length) {
+            var newSlots = new WgpuEffect[Math.Max(2 * slots.Length, kernelId + 1)];
+            Array.Copy(slots, newSlots, slots.Length);
+            slots = effectSlots = newSlots;
+        }
+        slots[kernelId] = new WgpuEffect(kernelId, wgslHash, pipeline, bufferLayout, uniformLayout);
+        return ref slots[kernelId];
+    }
+    
+    public void UpdateBufferCache(int slot, WgpuBindGroup bindGroup, ulong hash) {
+        effectSlots[slot].bufferCache.Update(bindGroup, hash);
+    }
+    
+    public WgpuShaderModule CreateShaderModule(ReadOnlySpan<byte> wgslSource, ReadOnlySpan<byte> shaderLabel)
+    {
+        fixed (byte* pShaderBytes = wgslSource)
+        fixed (byte* labelPtr = shaderLabel)
+        {
+            // create descriptor
+            var wgslDesc = new ShaderSourceWGSL {    	// was: new ShaderModuleWGSLDescriptor
+                code    = WgpuUtils.FromPtrSpan(pShaderBytes, wgslSource),
+                chain   = new ChainedStruct {
+                    sType   = SType.ShaderSourceWGSL	// was: SType.ShaderModuleWgsldescriptor
+                }
+            };
+            var desc = new ShaderModuleDescriptor {
+                label       = WgpuUtils.FromPtrSpan(labelPtr, shaderLabel),
+                nextInChain = (ChainedStruct*)&wgslDesc,
+            };
+            // Compile shader in driver
+            var handle = wgpuDeviceCreateShaderModule(DevicePtr, &desc);
+            errorHandler.ThrowOnError();
+            return new WgpuShaderModule(handle);
+        }
+    }
+    
+    public WgpuComputePipeline CreateComputePipeline(
+        WgpuShaderModule    module,
+        WgpuBindGroupLayout bufferLayout,
+        WgpuBindGroupLayout uniformLayout,
+        ReadOnlySpan<byte>  entryPoint)
+    {
+        Span<WgpuBindGroupLayout> layouts = stackalloc WgpuBindGroupLayout[2];
+        layouts[0] = bufferLayout;
+        layouts[1] = uniformLayout;
+        
+        fixed (byte*                pEntryPoint = entryPoint)
+        fixed (WgpuBindGroupLayout*  layoutsPtr  = layouts)
+        {
+            var label = WgpuUtils.FromPtrSpan(pEntryPoint, entryPoint);
+            var layoutDesc = new PipelineLayoutDescriptor {
+                label                   = label,
+                bindGroupLayoutCount    = 2,
+                bindGroupLayouts        = (BindGroupLayout**)layoutsPtr
+            };
+            var pipelineLayout = wgpuDeviceCreatePipelineLayout(DevicePtr, &layoutDesc);
+            try {
+                var computeDesc = new ComputePipelineDescriptor {
+                    label       = label,
+                    layout      = pipelineLayout,
+                    compute     = new ComputeState {	// was: new ProgrammableStageDescriptor
+                        module      = module.handle,
+                        entryPoint  = WgpuUtils.FromPtrSpan(pEntryPoint, entryPoint)
+                    }
+                };
+                var handle = wgpuDeviceCreateComputePipeline(DevicePtr, &computeDesc);
+                return new WgpuComputePipeline(handle);
+            } finally {
+                if (pipelineLayout != null) wgpuPipelineLayoutRelease(pipelineLayout);
+                if (module.handle  != null) wgpuShaderModuleRelease(module.handle);
+            }
+        }
+    }
+    
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public WgpuBindGroupLayout GetBindGroupLayout(ulong hashKey)
+    {
+        layoutCache.TryGetValue(hashKey, out WgpuBindGroupLayout layout);
+        return layout;
+    }
+
+    public WgpuBindGroupLayout CreateBindGroupLayout(ReadOnlySpan<WgpuLayoutEntry> entries, bool dynamicOffset, ulong hashKey, ReadOnlySpan<byte> layoutLabel)
+    {
+        Span<BindGroupLayoutEntry> nativeEntries = stackalloc BindGroupLayoutEntry[entries.Length];
+        
+        for (int i = 0; i < entries.Length; i++) {
+            nativeEntries[i] = new BindGroupLayoutEntry {
+                binding         = (uint)entries[i].Binding,
+                visibility      = (ulong)ShaderStage.Compute,
+                buffer          = new BufferBindingLayout {
+                    type                = entries[i].Type,
+                    hasDynamicOffset    = WgpuUtils.FromBool(dynamicOffset),    // true for uniform buffer
+                    minBindingSize      = 0                                     // 0: no validation of minimum size
+                }
+            };
+        }
+        fixed (byte*                    labelPtr    = layoutLabel)
+        fixed (BindGroupLayoutEntry*    entriesPtr  = nativeEntries)
+        {
+            var desc = new BindGroupLayoutDescriptor {
+                label       = WgpuUtils.FromPtrSpan(labelPtr, layoutLabel),
+                entryCount  = (uint)nativeEntries.Length,
+                entries     = entriesPtr,
+            };
+            var handle = wgpuDeviceCreateBindGroupLayout(DevicePtr, &desc);
+            if (handle == null)
+                throw new Exception("Failed to create BindGroupLayout. Check your Slot-indexes!");
+            
+            // Add new GpuBindGroupLayout to cache
+            var layout = new WgpuBindGroupLayout(handle);
+            layoutCache.Add(hashKey, layout);
+            return layout;
+        }
+    }
+}
--- src/Vectorization.WebGPU/API/Device.cs
+++ src/Vectorization.WebGPU/API/Device.cs
@@ -49,6 +49,7 @@ public sealed unsafe partial class WgpuDevice : GpuDevice
     internal            BufferEntry[]       bufferEntries       = [];   // ranges & segments per GpuBuffer
     private  readonly   WgpuIO              wgpuIO              = new ();
     
+    
     private sealed class WgpuDeviceDebugView(WgpuDevice device)
     {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
@@ -126,43 +127,6 @@ public sealed unsafe partial class WgpuDevice : GpuDevice
         isDisposed = true;
     }
  
-    // --- effectSlots
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ref WgpuEffect GetEffect(int slot, ulong wgslHash)
-    {
-        var slots = effectSlots;
-        if (slot < slots.Length) {
-            ref var effect = ref slots[slot];
-            if (effect.wgslHash == wgslHash) {
-                return ref effect;
-            }
-        }
-        return ref MissingEffect;
-    }
-    
-    private static WgpuEffect MissingEffect;
-    
-    public ref WgpuEffect CreateEffect(
-        int                     kernelId,
-        ulong                   wgslHash,
-        WgpuComputePipeline     pipeline,
-        WgpuBindGroupLayout     bufferLayout,
-        WgpuBindGroupLayout     uniformLayout)
-    {
-        var slots = effectSlots;
-        if (kernelId >= slots.Length) {
-            var newSlots = new WgpuEffect[Math.Max(2 * slots.Length, kernelId + 1)];
-            Array.Copy(slots, newSlots, slots.Length);
-            slots = effectSlots = newSlots;
-        }
-        slots[kernelId] = new WgpuEffect(kernelId, wgslHash, pipeline, bufferLayout, uniformLayout);
-        return ref slots[kernelId];
-    }
-    
-    public void UpdateBufferCache(int slot, WgpuBindGroup bindGroup, ulong hash) {
-        effectSlots[slot].bufferCache.Update(bindGroup, hash);
-    }
-
     internal WgpuDevice(
         string              label,
         WgpuErrorHandler    errorHandler,
@@ -371,6 +335,10 @@ public sealed unsafe partial class WgpuDevice : GpuDevice
         return gpuBuffer;
     }
     
+    // --- GpuDevice
+    protected override PipelineContext NewPipelineContext() => new CommandRecorder(this);
+    
+    
     // --- CommandStream
     [StackTraceHidden]
     internal void ValidateThreadSafety()
@@ -403,110 +371,6 @@ public sealed unsafe partial class WgpuDevice : GpuDevice
         } */
         var readSize = wgpuIO.Submit(null, this, null);
         wgpuIO.ReadBuffers(this, readSize);
-    }
-    
-    // ----------------------------- section "pure" methods used to create WebGPU structs ----------------------------- 
-    public WgpuShaderModule CreateShaderModule(ReadOnlySpan<byte> wgslSource, ReadOnlySpan<byte> shaderLabel)
-    {
-        fixed (byte* pShaderBytes = wgslSource)
-        fixed (byte* labelPtr = shaderLabel)
-        {
-            // create descriptor
-            var wgslDesc = new ShaderSourceWGSL {    	// was: new ShaderModuleWGSLDescriptor
-                code    = WgpuUtils.FromPtrSpan(pShaderBytes, wgslSource),
-                chain   = new ChainedStruct {
-                    sType   = SType.ShaderSourceWGSL	// was: SType.ShaderModuleWgsldescriptor
-                }
-            };
-            var desc = new ShaderModuleDescriptor {
-                label       = WgpuUtils.FromPtrSpan(labelPtr, shaderLabel),
-                nextInChain = (ChainedStruct*)&wgslDesc,
-            };
-            // Compile shader in driver
-            var handle = wgpuDeviceCreateShaderModule(DevicePtr, &desc);
-            errorHandler.ThrowOnError();
-            return new WgpuShaderModule(handle);
-        }
-    }
-    
-    public WgpuComputePipeline CreateComputePipeline(
-        WgpuShaderModule    module,
-        WgpuBindGroupLayout bufferLayout,
-        WgpuBindGroupLayout uniformLayout,
-        ReadOnlySpan<byte>  entryPoint)
-    {
-        Span<WgpuBindGroupLayout> layouts = stackalloc WgpuBindGroupLayout[2];
-        layouts[0] = bufferLayout;
-        layouts[1] = uniformLayout;
-        
-        fixed (byte*                pEntryPoint = entryPoint)
-        fixed (WgpuBindGroupLayout*  layoutsPtr  = layouts)
-        {
-            var label = WgpuUtils.FromPtrSpan(pEntryPoint, entryPoint);
-            var layoutDesc = new PipelineLayoutDescriptor {
-                label                   = label,
-                bindGroupLayoutCount    = 2,
-                bindGroupLayouts        = (BindGroupLayout**)layoutsPtr
-            };
-            var pipelineLayout = wgpuDeviceCreatePipelineLayout(DevicePtr, &layoutDesc);
-            try {
-                var computeDesc = new ComputePipelineDescriptor {
-                    label       = label,
-                    layout      = pipelineLayout,
-                    compute     = new ComputeState {	// was: new ProgrammableStageDescriptor
-                        module      = module.handle,
-                        entryPoint  = WgpuUtils.FromPtrSpan(pEntryPoint, entryPoint)
-                    }
-                };
-                var handle = wgpuDeviceCreateComputePipeline(DevicePtr, &computeDesc);
-                return new WgpuComputePipeline(handle);
-            } finally {
-                if (pipelineLayout != null) wgpuPipelineLayoutRelease(pipelineLayout);
-                if (module.handle  != null) wgpuShaderModuleRelease(module.handle);
-            }
-        }
-    }
-    
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public WgpuBindGroupLayout GetBindGroupLayout(ulong hashKey)
-    {
-        layoutCache.TryGetValue(hashKey, out WgpuBindGroupLayout layout);
-        return layout;
-    }
-
-    public WgpuBindGroupLayout CreateBindGroupLayout(ReadOnlySpan<WgpuLayoutEntry> entries, bool dynamicOffset, ulong hashKey, ReadOnlySpan<byte> layoutLabel)
-    {
-        Span<BindGroupLayoutEntry> nativeEntries = stackalloc BindGroupLayoutEntry[entries.Length];
-        
-        for (int i = 0; i < entries.Length; i++) {
-            nativeEntries[i] = new BindGroupLayoutEntry {
-                binding         = (uint)entries[i].Binding,
-                visibility      = (ulong)ShaderStage.Compute,
-                buffer          = new BufferBindingLayout {
-                    type                = entries[i].Type,
-                    hasDynamicOffset    = WgpuUtils.FromBool(dynamicOffset),    // true for uniform buffer
-                    minBindingSize      = 0                                     // 0: no validation of minimum size
-                }
-            };
-        }
-        fixed (byte*                    labelPtr    = layoutLabel)
-        fixed (BindGroupLayoutEntry*    entriesPtr  = nativeEntries)
-        {
-            var desc = new BindGroupLayoutDescriptor {
-                label       = WgpuUtils.FromPtrSpan(labelPtr, layoutLabel),
-                entryCount  = (uint)nativeEntries.Length,
-                entries     = entriesPtr,
-            };
-            var handle = wgpuDeviceCreateBindGroupLayout(DevicePtr, &desc);
-            if (handle == null)
-                throw new Exception("Failed to create BindGroupLayout. Check your Slot-indexes!");
-            
-            // Add new GpuBindGroupLayout to cache
-            var layout = new WgpuBindGroupLayout(handle);
-            layoutCache.Add(hashKey, layout);
-            return layout;
-        }
     }
 }
 
