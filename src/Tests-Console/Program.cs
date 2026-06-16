dc6f60e2edcc9cee818ed713a902a245c451e792
friflo <ullrich@praetz.de>
2026-06-16 10:41:46 +0200

    console - activate RenderTest

--- src/Tests-Console/Program.cs
+++ src/Tests-Console/Program.cs
@@ -7,7 +7,7 @@ using Friflo.Vectorization.WebGPU.Runtime;
 using TestConsole;
 
 
-// new SdlWindow().Main(); return;
+new SdlWindow().Main(); return;
 
 // using var instance    = CpuInstance.CreateInstance();
 // using var adapter     = instance.CreateAdapter(GpuBackendType.SIMD);
