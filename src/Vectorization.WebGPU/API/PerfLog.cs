de25a91af0c0da8c4a26f518064baedce1271ebe
friflo <ullrich@praetz.de>
2026-06-20 10:48:34 +0200

    wgpu - used public properties in PerfLog

--- src/Vectorization.WebGPU/API/PerfLog.cs
+++ src/Vectorization.WebGPU/API/PerfLog.cs
@@ -5,38 +5,41 @@ using System;
 using System.IO;
 using System.Text;
 
+// ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable once CheckNamespace
 namespace Friflo.Vectorization.WebGPU;
 
 public sealed class PerfLog
 {
-    private long                            memoryAllocated;
-    private int                             frameCount;
-    private static readonly StringBuilder   Builder = new(256);
-    private static readonly StreamWriter    Writer  = new(Console.OpenStandardOutput(), Encoding.UTF8, 256);
+    public              long            MemoryAllocated { get; private set; }
+    public              int             FrameCount      { get; private set; }
+    public  readonly    StringBuilder   Builder         = new(256);
+    private readonly    StreamWriter    writer          = new(Console.OpenStandardOutput(), Encoding.UTF8, 256);
 
     public void Flush()
     {
-        foreach (var chunk in Builder.GetChunks()) {
-            Writer.Write(chunk.Span);
+        if (Builder.Length == 0) {
+            return;
         }
-        Writer.Flush();
+        foreach (var chunk in Builder.GetChunks()) {
+            writer.Write(chunk.Span);
+        }
+        writer.Flush();
         Builder.Clear();
     }
         
     public void Trace(int lapCount)
     {
-        if (++frameCount % lapCount == 0) {
+        if (++FrameCount % lapCount == 0) {
             Builder.Append("frame: ");
-            Builder.Append(frameCount);
+            Builder.Append(FrameCount);
             Builder.AppendLine();
-            Flush();
         }
         var cur = GC.GetAllocatedBytesForCurrentThread();
-        if (cur != memoryAllocated) {
-            Builder.AppendLine($"memory allocations: {cur - memoryAllocated} ");
-            Flush();
+        if (cur != MemoryAllocated) {
+            Builder.AppendLine($"memory allocations: {cur - MemoryAllocated} ");
         }
-        memoryAllocated = GC.GetAllocatedBytesForCurrentThread();
+        Flush();
+        MemoryAllocated = GC.GetAllocatedBytesForCurrentThread();
     }
 }
