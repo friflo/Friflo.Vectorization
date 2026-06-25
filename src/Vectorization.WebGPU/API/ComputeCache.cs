f56e4c0654dea6c78e9727b84b208a833a57a0a3
friflo <ullrich@praetz.de>
2026-06-25 09:25:13 +0200

    wgpu - cleanup

--- src/Vectorization.WebGPU/API/ComputeCache.cs
+++ src/Vectorization.WebGPU/API/ComputeCache.cs
@@ -1,12 +1,8 @@
 ﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Friflo.Vectorization.WebGPU.Runtime;
-using static Friflo.Vectorization.WebGPU.Runtime.WebGPU_native;
 
 // ReSharper disable InconsistentNaming
 // ReSharper disable CheckNamespace
