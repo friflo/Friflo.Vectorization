a5de51cff3adfb2dc36847a47655e3abe3e080b4
friflo <ullrich@praetz.de>
2026-06-25 14:20:21 +0200

    wgpu - move CreateTexture2D() -> Texture.Create.cs

---
+++ src/Vectorization.WebGPU/API/Texture.Create.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using Friflo.Vectorization.WebGPU.Runtime;
+using static Friflo.Vectorization.WebGPU.Runtime.WebGPU_native;
+
+// ReSharper disable CheckNamespace
+namespace Friflo.Vectorization.WebGPU;
+
+
+public sealed unsafe partial class WgpuDevice
+{
+    public GpuTexture2D CreateTexture2D(int width, int height, TextureFormat format, TextureUsage usage, string label = null, in TextureOptions? options = null, Span<TextureFormat> viewFormats = default)
+    {
+        var desc = new TextureDescriptor();
+        SetTextureDescriptor(ref desc, TextureDimension.D2D, width, height, format, usage, in options, viewFormats.Length);
+        
+        int     labelMaxCount   = WgpuUtils.GetMaxCount(label);
+        byte*   labelBuffer     = stackalloc byte[labelMaxCount];
+        desc.label              = WgpuUtils.CopyToStringView(label, labelBuffer, labelMaxCount);
+        
+        fixed(TextureFormat* ptr = viewFormats) {
+            desc.viewFormats = ptr;
+            Texture* texture = wgpuDeviceCreateTexture(DevicePtr, &desc);
+            return new GpuTexture2D(this, desc, texture, label);
+        }
+    }
+}
--- src/Vectorization.WebGPU/API/Texture.Device.cs
+++ src/Vectorization.WebGPU/API/Texture.Device.cs
@@ -4,12 +4,11 @@
 
 using System;
 using Friflo.Vectorization.WebGPU.Runtime;
-using static Friflo.Vectorization.WebGPU.Runtime.WebGPU_native;
 
+// ReSharper disable InconsistentNaming
 // ReSharper disable UnassignedField.Global
 // ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable ConvertToConstant.Global
-// ReSharper disable InconsistentNaming
 // ReSharper disable CheckNamespace
 namespace Friflo.Vectorization.WebGPU;
 
@@ -55,21 +54,5 @@ public sealed unsafe partial class WgpuDevice
     public GpuTexture2D CreateTexture(int width, int height, TextureFormat format, TextureUsage usage, string label = null, in TextureOptions? options = null, Span<TextureFormat> viewFormats = default)
     {
         return CreateTexture2D(width, height, format, usage, label, in options, viewFormats);
-    }
-    
-    public GpuTexture2D CreateTexture2D(int width, int height, TextureFormat format, TextureUsage usage, string label = null, in TextureOptions? options = null, Span<TextureFormat> viewFormats = default)
-    {
-        var desc = new TextureDescriptor();
-        SetTextureDescriptor(ref desc, TextureDimension.D2D, width, height, format, usage, in options, viewFormats.Length);
-        
-        int     labelMaxCount   = WgpuUtils.GetMaxCount(label);
-        byte*   labelBuffer     = stackalloc byte[labelMaxCount];
-        desc.label              = WgpuUtils.CopyToStringView(label, labelBuffer, labelMaxCount);
-        
-        fixed(TextureFormat* ptr = viewFormats) {
-            desc.viewFormats = ptr;
-            Texture* texture = wgpuDeviceCreateTexture(DevicePtr, &desc);
-            return new GpuTexture2D(this, desc, texture, label);
-        }
     }
 }
