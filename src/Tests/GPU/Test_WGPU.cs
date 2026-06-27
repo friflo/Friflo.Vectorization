d11d94c0d8946535e07f4f19de0797fa0b24af0e
friflo <ullrich@praetz.de>
2026-06-27 07:42:40 +0200

    tests - add Test_WGPU_RenderConfig() assertion

--- src/Tests/GPU/Test_WGPU.cs
+++ src/Tests/GPU/Test_WGPU.cs
@@ -16,14 +16,17 @@ public static class Test_WGPU
                 constants = [new WgpuConstantEntry { key = "test", value = 123 }] // add test entry    
             }
         };
-        var config = descSrc.CreateConfig("config");
+        Assert.IsFalse(descSrc.MultisampleState.alphaToCoverageEnabled);
         
+        var config = descSrc.CreateConfig("config");
         Assert.AreEqual("config", config.Name);
+        
+        descSrc.MultisampleState.alphaToCoverageEnabled = true;
+        Assert.IsFalse(config.Descriptor.MultisampleState.alphaToCoverageEnabled); // config.Descriptor stays unchanged
         
         ref readonly var desc = ref config.Descriptor;
         Assert.AreEqual(1, desc.FragmentState!.Value.constants.Length);
         
-        descSrc.MultisampleState.alphaToCoverageEnabled = true;
         var mutatedConfig = descSrc.CreateConfig("mutatedConfig");
         Assert.AreEqual("mutatedConfig", mutatedConfig.Name);
         
