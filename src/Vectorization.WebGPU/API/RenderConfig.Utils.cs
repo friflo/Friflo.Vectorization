32b563137871b645441670428d64058c9e9a5337
friflo <ullrich@praetz.de>
2026-06-27 08:22:13 +0200

    wgpu - add ValueNullable<> struct

--- src/Vectorization.WebGPU/API/RenderConfig.Utils.cs
+++ src/Vectorization.WebGPU/API/RenderConfig.Utils.cs
@@ -4,10 +4,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using Friflo.Vectorization.WebGPU.Runtime;
+// ReSharper disable ConvertToPrimaryConstructor
 
 // ReSharper disable SuggestVarOrType_BuiltInTypes
 // ReSharper disable InconsistentNaming
@@ -86,6 +88,82 @@ public static class ValueArrayBuilder
         return new ValueArray<T>(items);
     }
 }
+
+// -------------------------------------- ValueNullable<T> --------------------------------------
+/// <summary>
+/// An immutable, structural value-comparable wrapper around a nullable value.
+/// </summary>
+/// <remarks>
+/// Ensures strict content-based equality checking (<see langword="Equals"/> and <see langword="GetHashCode"/>) 
+/// inside <see langword="struct"/> configurations.
+/// </remarks>
+/// <typeparam name="T">The unmanaged value type of the underlying element.</typeparam>
+public readonly struct ValueNullable<T> : IEquatable<ValueNullable<T>> where T : struct
+{
+    private readonly bool   _hasValue;
+    private readonly T      _value;
+
+    public bool HasValue => _hasValue;
+
+    [UnscopedRef]
+    public ref readonly T Value {
+        get {
+            if (_hasValue) {
+                return ref _value;
+            }
+            throw new InvalidOperationException("Nullable object must have a value.");
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ValueNullable(in T value)
+    {
+        _value      = value;
+        _hasValue   = true;
+    }
+
+    /// <summary>Retrieves the value of the current instance, or the default value of the underlying type.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T GetValueOrDefault() => _value;
+
+    /// <summary>Retrieves the value of the current instance, or the specified default value.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T GetValueOrDefault(in T defaultValue) => _hasValue ? _value : defaultValue;
+
+    public override string ToString() => _hasValue ? _value.ToString() : string.Empty;
+
+    // --- Structural Equality Implementation
+    public bool Equals(ValueNullable<T> other)
+    {
+        if (_hasValue != other._hasValue) return false;
+        if (!_hasValue) return true;
+        return _value.Equals(other._value);
+    }
+
+    public override bool Equals(object obj) => obj is ValueNullable<T> other && Equals(other);
+
+    public override int GetHashCode()
+    {
+        if (!_hasValue) return 0;
+        return _value.GetHashCode();
+    }
+
+    // --- Operator Overloads & Implicit Conversions (Syntax Sugar)
+    
+    /// <summary>Allows direct assignment from a pure value: <c>ValueNullable&lt;int&gt; val = 5;</c></summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator ValueNullable<T>(in T value) => new(value);
+
+    /// <summary>Seamlessly bridges the gap from native C# nullable syntax: <c>int? x = 5; ValueNullable&lt;int&gt; y = x;</c></summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator ValueNullable<T>(in T? nativeNullable) => 
+        nativeNullable.HasValue ? new ValueNullable<T>(nativeNullable.Value) : default;
+
+
+    public static bool operator ==(in ValueNullable<T> left, in ValueNullable<T> right) =>  left.Equals(right);
+    public static bool operator !=(in ValueNullable<T> left, in ValueNullable<T> right) => !left.Equals(right);
+}
+
 
 
 // -------------------------------------- NativeAllocator --------------------------------------
