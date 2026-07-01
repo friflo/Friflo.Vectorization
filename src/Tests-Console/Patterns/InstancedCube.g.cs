acb804dc3a066298ff91cfb9b120c69920032612
friflo <ullrich@praetz.de>
2026-07-01 14:40:28 +0200

    console - InstancedCube: resolved TODO

--- src/Tests-Console/Patterns/InstancedCube.g.cs
+++ src/Tests-Console/Patterns/InstancedCube.g.cs
@@ -46,7 +46,7 @@ public partial class InstancedCube
         
         int verticesBufferCount = pass_.SetVertexBuffer(config, 0, verticesBuffer); // slot: 0 - [VertexBuffer(0)]  references:  desc.VertexState.buffers[0]
    
-        pass_.Draw(verticesBufferCount, 16, 0, 0);  // TODO calculate 16
+        pass_.Draw(verticesBufferCount, verticesBuffer.Length, 0, 0);
 	}
     
     private sealed class TextureTest_GPU_Cache : BindGroupCache
