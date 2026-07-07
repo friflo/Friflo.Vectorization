ce4590c07d5ecc8f9a28c9d3891cdc2fe3ae66dc
friflo <ullrich@praetz.de>
2026-07-07 06:26:09 +0200

    wgpu - moved WgpuResource to WebGPU.Runtime

--- src/Vectorization.WebGPU/API/RenderPass.cs
+++ src/Vectorization.WebGPU/API/RenderPass.cs
@@ -5,8 +5,6 @@
 using System;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Friflo.Vectorization.GPU;
@@ -307,60 +305,4 @@ public readonly unsafe struct WgpuRenderPipeline
         this.handle = handle;
     }
 }
-
-public static class WgpuResource
-{
-     
-    public static ReadOnlySpan<byte> GetResource(Type type, string resourcePath)
-    {
-        return GetResource(type.Assembly, resourcePath);
-    }
-     
-    private static unsafe ReadOnlySpan<byte> GetResource(Assembly assembly, string resourcePath)
-    {
-#if DEBUG
-        var res = GetResourceFromFile(assembly, resourcePath);
-        if (res.Length != 0) {
-            return res;
-        }
-#endif
-        var assemblyName = assembly.GetName().Name;
-        var resourceName = $"{assemblyName}.{resourcePath.Replace('/', '.')}";
-        
-        var stream = assembly.GetManifestResourceStream(resourceName);
-        if (stream == null) { 
-            throw new FileNotFoundException($"Resource '{resourceName}' not found");
-        }
-        if (stream is UnmanagedMemoryStream unmanagedStream)
-        {
-            var span = new ReadOnlySpan<byte>(unmanagedStream.PositionPointer, (int)unmanagedStream.Length);
-        
-            // Detect UTF-8 BOM and skip
-            if (span.Length >= 3 && span[0] == 0xEF && span[1] == 0xBB && span[2] == 0xBF) {
-                return span.Slice(3);
-            }
-            return span;
-        }
-        throw new InvalidOperationException($"Resource '{resourceName}' not found");
-    }
-    
-    private static ReadOnlySpan<byte> GetResourceFromFile(Assembly assembly, string resourceName)
-    {
-        // var cleanPath       = resourceName.Replace(assembly.GetName().Name + ".", "");
-        // int lastDot         = cleanPath.LastIndexOf('.');
-        // var relativePath    = cleanPath.Substring(0, lastDot).Replace('.', '/') + cleanPath.Substring(lastDot);
-        var fullPath = Path.Combine(AppContext.BaseDirectory, "../../../", resourceName);
-
-        if (File.Exists(fullPath))
-        {
-            var bytes = File.ReadAllBytes(fullPath);
-            return (bytes.Length >= 3 && bytes[0] == 0xEF && bytes[1] == 0xBB && bytes[2] == 0xBF) 
-                ? bytes.AsSpan(3) : bytes;
-        }
-        return default;
-    }
-}
-
-
-
 
---
+++ src/Vectorization.WebGPU/Runtime/Resource.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Reflection;
+
+// ReSharper disable InvertIf
+// ReSharper disable once CheckNamespace
+namespace Friflo.Vectorization.WebGPU.Runtime;
+
+
+public static class WgpuResource
+{
+     
+    public static ReadOnlySpan<byte> GetResource(Type type, string resourcePath)
+    {
+        return GetResource(type.Assembly, resourcePath);
+    }
+     
+    private static unsafe ReadOnlySpan<byte> GetResource(Assembly assembly, string resourcePath)
+    {
+#if DEBUG
+        var res = GetResourceFromFile(assembly, resourcePath);
+        if (res.Length != 0) {
+            return res;
+        }
+#endif
+        var assemblyName = assembly.GetName().Name;
+        var resourceName = $"{assemblyName}.{resourcePath.Replace('/', '.')}";
+        
+        var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream == null) { 
+            throw new FileNotFoundException($"Resource '{resourceName}' not found");
+        }
+        if (stream is UnmanagedMemoryStream unmanagedStream)
+        {
+            var span = new ReadOnlySpan<byte>(unmanagedStream.PositionPointer, (int)unmanagedStream.Length);
+        
+            // Detect UTF-8 BOM and skip
+            if (span.Length >= 3 && span[0] == 0xEF && span[1] == 0xBB && span[2] == 0xBF) {
+                return span.Slice(3);
+            }
+            return span;
+        }
+        throw new InvalidOperationException($"Resource '{resourceName}' not found");
+    }
+    
+    private static ReadOnlySpan<byte> GetResourceFromFile(Assembly assembly, string resourceName)
+    {
+        // var cleanPath       = resourceName.Replace(assembly.GetName().Name + ".", "");
+        // int lastDot         = cleanPath.LastIndexOf('.');
+        // var relativePath    = cleanPath.Substring(0, lastDot).Replace('.', '/') + cleanPath.Substring(lastDot);
+        var fullPath = Path.Combine(AppContext.BaseDirectory, "../../../", resourceName);
+
+        if (File.Exists(fullPath))
+        {
+            var bytes = File.ReadAllBytes(fullPath);
+            return (bytes.Length >= 3 && bytes[0] == 0xEF && bytes[1] == 0xBB && bytes[2] == 0xBF) 
+                ? bytes.AsSpan(3) : bytes;
+        }
+        return default;
+    }
+}
+
