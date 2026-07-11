5c305ffdf5befb214d7fefc827e4b7ec1e3c9a95
friflo <ullrich@praetz.de>
2026-07-11 15:06:41 +0200

    Tests - extract WgslUtils

--- src/Tests/WGSL/Tests_WGSL.cs
+++ src/Tests/WGSL/Tests_WGSL.cs
@@ -1,78 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.IO;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using Friflo.Vectorization.WebGPU;
+﻿using Friflo.Vectorization.WebGPU;
 using Friflo.WGSL.Transpiler.CodeFixes;
-using Friflo.WGSL.Transpiler.CSharp;
 using NUnit.Framework;
 
-// ReSharper disable UseCollectionExpression
-namespace Tests.WGSL;
 
 // ReSharper disable once InconsistentNaming
+namespace Tests.WGSL;
+
 public static class Tests_WGSL
 {
-    // Pattern: [RootNamespace].[Folder].[Filename].[Extension]
-    // E.g.    "Tests.shaders.triangle.wgsl"
-    private static string ReadWgslResource(string resourceName)
-    {
-        var assembly = Assembly.GetExecutingAssembly();
-        
-        using var stream = assembly.GetManifestResourceStream(resourceName);
-        Assert.NotNull(stream);
-
-        using var reader = new StreamReader(stream);
-        return reader.ReadToEnd();
-    }
-    
-    public static (CsMethod, ImmutableArray<WgslFile>) GetShaders(Type type, [CallerMemberName] string callerName = "")
-    {
-        var methodInfo = type.GetMethod(callerName);
-        if (methodInfo == null) throw new InvalidOperationException("Could not find method " + callerName);
-        
-        var files           = new List<WgslFile>();
-        var shaders         = new List<CsShader>();
-        var attributesData  = methodInfo.GetCustomAttributesData();
-        
-        foreach (var data in attributesData)
-        {
-            if (data.AttributeType != typeof(ShaderAttribute)) continue;
-            var args = data.ConstructorArguments;
-            var path = (string)args[0].Value;
-            if (!path!.StartsWith("~/")) throw new InvalidOperationException("expect path starts with ~/ - path:" + path);
-            
-            path = path.Substring(2);
-            var resourceName = "Tests." + path.Replace('/', '.'); 
-            var wgsl = ReadWgslResource(resourceName);
-            files.Add(new WgslFile { NormalizedPath = path, Content = wgsl, Hash = 0 });
-            shaders.Add(new CsShader {
-                path = path,
-                frag = args[1].Value as string,
-                vert = args[2].Value as string,
-            });
-        }
-        var method = new CsMethod {
-            Name            = "",
-            Hash            = "",
-            DeclaringType   = default,
-            Parameters      = default,
-            DrawVertexIndex = null,
-            Modifier        = default,
-            Shaders         = shaders.ToValueArray(),
-            TypeInfos       = default
-        };
-        return (method, files.ToImmutableArray());
-    }
-    
     
     [Test]
     [Shader("~/shaders/triangle.wgsl")]
     public static void Tests_WGSL_Parse_triangle()
     {
-        var (_, files) = GetShaders(typeof(Tests_WGSL));
+        var (_, files) = WgslUtils.GetShaders(typeof(Tests_WGSL));
         var metadata = WgslSuperpowerParser.ParseShader(files[0].Content);
         
         Assert.AreEqual(4, metadata.Structs.Count);
@@ -80,11 +21,12 @@ public static class Tests_WGSL
         Assert.AreEqual(2, metadata.Bindings.Count);
     }
     
+    
     [Test]
     [Shader("~/shaders/raymarcher_no_texture.wgsl")]
     public static void Tests_WGSL_Parse_raymarcher_no_texture()
     {
-        var (_, files) = GetShaders(typeof(Tests_WGSL));
+        var (_, files) = WgslUtils.GetShaders(typeof(Tests_WGSL));
         var metadata = WgslSuperpowerParser.ParseShader(files[0].Content);
         
         Assert.AreEqual(4, metadata.Structs.Count);
@@ -92,11 +34,12 @@ public static class Tests_WGSL
         Assert.AreEqual(1, metadata.Bindings.Count);
     }
     
+    
     [Test]
 	[Shader("~/shaders/triangle.wgsl", vertex: "vs_main", fragment: "fs_main")]
     public static void Tests_WGSL_GenerateParameters()
     {
-        var (method, files) = GetShaders(typeof(Tests_WGSL));
+        var (method, files) = WgslUtils.GetShaders(typeof(Tests_WGSL));
         var result = CodeFixer.CreateShaderParams(method, files);
         
         Assert.That(result.Errors.Length, Is.EqualTo(0));
@@ -108,12 +51,13 @@ public static class Tests_WGSL
             """));
     }
     
+    
     [Test]
 	[Shader("~/shaders/shadowMapping/vertex.wgsl",    vertex:   "main")]
 	[Shader("~/shaders/shadowMapping/fragment.wgsl",  fragment: "main")]
     public static void Tests_WGSL_GenerateSamplerTextureView()
     {
-        var (method, files) = GetShaders(typeof(Tests_WGSL));
+        var (method, files) = WgslUtils.GetShaders(typeof(Tests_WGSL));
         var result = CodeFixer.CreateShaderParams(method, files);
         
         Assert.That(result.Errors.Length, Is.EqualTo(0));
@@ -133,7 +77,7 @@ public static class Tests_WGSL
 	[Shader("~/shaders/sampleTextureMixColor.frag.wgsl",  fragment: "main")]
     public static void Tests_WGSL_Generate_texture_2d()
     {
-        var (method, files) = GetShaders(typeof(Tests_WGSL));
+        var (method, files) = WgslUtils.GetShaders(typeof(Tests_WGSL));
         var result = CodeFixer.CreateShaderParams(method, files);
         
         Assert.That(result.Errors.Length, Is.EqualTo(0));
@@ -145,5 +89,4 @@ public static class Tests_WGSL
                     [texture_2d(0, 2, ST.f32)]    GpuTextureView myTexture)
             """));
     }
-    
 }
---
+++ src/Tests/WGSL/WgslUtils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Friflo.Vectorization.WebGPU;
+using Friflo.WGSL.Transpiler.CSharp;
+using NUnit.Framework;
+
+
+// ReSharper disable UseCollectionExpression
+// ReSharper disable once InconsistentNaming
+namespace Tests.WGSL;
+
+internal static class WgslUtils
+{
+    // Pattern: [RootNamespace].[Folder].[Filename].[Extension]
+    // E.g.    "Tests.shaders.triangle.wgsl"
+    private static string ReadWgslResource(string resourceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        Assert.NotNull(stream);
+
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+    
+    public static (CsMethod, ImmutableArray<WgslFile>) GetShaders(Type type, [CallerMemberName] string callerName = "")
+    {
+        var methodInfo = type.GetMethod(callerName);
+        if (methodInfo == null) throw new InvalidOperationException("Could not find method " + callerName);
+        
+        var files           = new List<WgslFile>();
+        var shaders         = new List<CsShader>();
+        var attributesData  = methodInfo.GetCustomAttributesData();
+        
+        foreach (var data in attributesData)
+        {
+            if (data.AttributeType != typeof(ShaderAttribute)) continue;
+            var args = data.ConstructorArguments;
+            var path = (string)args[0].Value;
+            if (!path!.StartsWith("~/")) throw new InvalidOperationException("expect path starts with ~/ - path:" + path);
+            
+            path = path.Substring(2);
+            var resourceName = "Tests." + path.Replace('/', '.'); 
+            var wgsl = ReadWgslResource(resourceName);
+            files.Add(new WgslFile { NormalizedPath = path, Content = wgsl, Hash = 0 });
+            shaders.Add(new CsShader {
+                path = path,
+                frag = args[1].Value as string,
+                vert = args[2].Value as string,
+            });
+        }
+        var method = new CsMethod {
+            Name            = "",
+            Hash            = "",
+            DeclaringType   = default,
+            Parameters      = default,
+            DrawVertexIndex = null,
+            Modifier        = default,
+            Shaders         = shaders.ToValueArray(),
+            TypeInfos       = default
+        };
+        return (method, files.ToImmutableArray());
+    }
+}
