f957e95299e572dde3b71edd9322737be20408fd
friflo <ullrich@praetz.de>
2026-07-12 10:35:11 +0200

    codeFixer - simplify AppendParameters()

--- src/Friflo.WGSL.Transpiler/CodeFixes/MethodParam.cs
+++ src/Friflo.WGSL.Transpiler/CodeFixes/MethodParam.cs
@@ -37,45 +37,31 @@ public static partial class CodeFixer
     {
         if (parameters.Count == 0) return;
 
-        int maxMapLength = 0;
         int maxAttrLength = 0;
 
-        var mapStrings = new string[parameters.Count];
-        for (int i = 0; i < parameters.Count; i++)
-        {
-            var param = parameters[i];
-            var bg = param.bindGroup;
-            string mapStr = bg.binding >= 10 ? $"[Map({bg.group},{bg.binding})]" : $"[Map({bg.group}, {bg.binding})]";
-            mapStrings[i] = mapStr;
-
-            if (mapStr.Length > maxMapLength) maxMapLength = mapStr.Length;
+        foreach (var param in parameters) {
             if (param.attribute.Length > maxAttrLength) maxAttrLength = param.attribute.Length;
         }
-
-        int mapTargetWidth  = ((maxMapLength + 3) / 4) * 4;
         int attrTargetWidth = maxAttrLength + 4; 
 
-        for (int i = 0; i < parameters.Count; i++)
+        foreach (var parameter in parameters)
         {
-            var bg = parameters[i];
-            string mapStr = mapStrings[i];
-
             sb.Append("        "); 
 
-            // column 1: [Map]
-            sb.Append(mapStr);
-            sb.Append(' ', mapTargetWidth - mapStr.Length);
+            // column 1: [Map(0, 1)]
+            var bg = parameter.bindGroup;
+            sb.Append($"[Map({bg.group},{bg.binding,2})] ");
 
             // column 2: WGSL-attribute (e.g. [texture_2d(ST.f32)])
-            sb.Append(bg.attribute);
+            sb.Append(parameter.attribute);
             
             // dynamic padding
-            sb.Append(' ', attrTargetWidth - bg.attribute.Length);
+            sb.Append(' ', attrTargetWidth - parameter.attribute.Length);
 
             // column 3 & 4: C#-Type and Name
-            sb.Append(bg.type);
+            sb.Append(parameter.type);
             sb.Append(' ');
-            sb.Append(bg.parameter);
+            sb.Append(parameter.parameter);
             sb.Append(",\n");
         }
     }
