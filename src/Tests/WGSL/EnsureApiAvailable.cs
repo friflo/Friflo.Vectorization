55c5e8870f2ab5be1c7a0a7bef3d83d291a5643a
friflo <ullrich@praetz.de>
2026-07-14 12:20:40 +0200

    tests - add static class EnsureApiAvailable

---
+++ src/Tests/WGSL/EnsureApiAvailable.cs
@@ -0,0 +1,18 @@
+﻿using Friflo.Vectorization.WebGPU;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+namespace Tests.WGSL;
+
+
+public static class EnsureApiAvailable
+{
+    public static void EnsureRenderPassApi(in RenderFrame frame)
+    {
+        using var pass = frame.BeginRenderPass(default);
+        pass.SetBlendConstant([1,2,3,4]);
+        pass.SetScissorRect(1,2,3,4);
+        pass.SetViewport(0,1,2,3,4,5);
+        pass.SetStencilReference(1);
+    }
+}
