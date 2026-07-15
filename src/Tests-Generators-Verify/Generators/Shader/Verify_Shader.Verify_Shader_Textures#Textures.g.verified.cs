d83ebfeb46adb44cfb33ddb68d3736e4a7522452
friflo <ullrich@praetz.de>
2026-07-15 08:13:43 +0200

    Verify - add test: Verify_Shader_Textures()

---
+++ src/Tests-Generators-Verify/Generators/Shader/Verify_Shader.Verify_Shader_Textures#Textures.g.verified.cs
@@ -0,0 +1,147 @@
+﻿//HintName: VerifyShader/ShaderExample/Textures.g.cs
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Friflo.Vectorization.GPU;
+using Friflo.Vectorization.GPU.Runtime;
+using Friflo.Vectorization.WebGPU;
+using Friflo.Vectorization.WebGPU.Runtime;
+
+namespace VerifyShader;
+
+public partial class ShaderExample
+{
+    private static partial void Textures(
+        RenderPass                  pass,
+        RenderConfig                config,
+        GpuTextureView              texture0,
+        GpuTextureView              texture1,
+        GpuTextureView              texture2,
+        GpuTextureView              texture3,
+        GpuTextureView              texture4,
+        GpuTextureView              texture5,
+        GpuTextureView              texture6,
+        GpuTextureView              texture8,
+        GpuTextureView              texture9,
+        GpuTextureView              texture10,
+        GpuTextureView              texture11,
+        GpuTextureView              texture12,
+        GpuTextureView              texture13,
+        GpuTextureView              texture14,
+        GpuTextureView              texture15,
+        GpuSampler                  sampler0,
+        GpuSampler                  sampler1)
+    {
+
+        var pass_       = pass.Internal;
+		var recorder	= pass_.Recorder;
+		recorder.Init(_Textures_GPU_ShaderId, "Textures_encoder"u8);
+
+        
+        ref readonly var pipelineCache = ref recorder.Device.GetPipelineCache(_Textures_GPU_ShaderId, config, _Textures_GPU_WgslHash);
+        if (!pipelineCache.IsCreated) {
+            pipelineCache = ref _Textures_GPU_CreatePipelineCache(recorder.Device, config);
+        }
+        pass_.SetPipeline(pipelineCache.renderPipeline);
+        
+        var bindGroupCache = (_Textures_GPU_Cache)pipelineCache.bindGroupCache;
+
+        // --- bind group 0
+        var key_0 = (texture0.Handle, texture1.Handle, texture2.Handle, texture3.Handle, texture4.Handle, texture5.Handle, texture6.Handle, texture8.Handle, texture9.Handle, texture10.Handle, texture11.Handle, texture12.Handle, texture13.Handle, texture14.Handle, texture15.Handle);
+        if (!bindGroupCache.bindGroup0.TryGetValue(key_0, out var bindGroup0)) {
+            recorder.BindGroupEntryTexture(texture0);
+            recorder.BindGroupEntryTexture(texture1);
+            recorder.BindGroupEntryTexture(texture2);
+            recorder.BindGroupEntryTexture(texture3);
+            recorder.BindGroupEntryTexture(texture4);
+            recorder.BindGroupEntryTexture(texture5);
+            recorder.BindGroupEntryTexture(texture6);
+            recorder.BindGroupEntryTexture(texture8);
+            recorder.BindGroupEntryTexture(texture9);
+            recorder.BindGroupEntryTexture(texture10);
+            recorder.BindGroupEntryTexture(texture11);
+            recorder.BindGroupEntryTexture(texture12);
+            recorder.BindGroupEntryTexture(texture13);
+            recorder.BindGroupEntryTexture(texture14);
+            recorder.BindGroupEntryTexture(texture15);
+            bindGroup0 = recorder.CreateBindGroup(pipelineCache.layouts[0], "Textures_bindGroup0"u8);
+            bindGroupCache.bindGroup0.Add(key_0, bindGroup0);
+        }
+        pass_.SetBindGroup(0, bindGroup0);
+        
+        // --- bind group 1
+        var key_1 = (sampler0.Handle, sampler1.Handle);
+        if (!bindGroupCache.bindGroup1.TryGetValue(key_1, out var bindGroup1)) {
+            recorder.BindGroupEntrySampler(sampler0);
+            recorder.BindGroupEntrySampler(sampler1);
+            bindGroup1 = recorder.CreateBindGroup(pipelineCache.layouts[1], "Textures_bindGroup1"u8);
+            bindGroupCache.bindGroup1.Add(key_1, bindGroup1);
+        }
+        pass_.SetBindGroup(1, bindGroup1);
+        
+        // --- draw
+    }
+
+    private sealed class _Textures_GPU_Cache : BindGroupCache
+    {
+        internal readonly   Dictionary<(nint, nint, nint, nint, nint, nint, nint, nint, nint, nint, nint, nint, nint, nint, nint), WgpuBindGroup>    bindGroup0 = new ();
+        internal readonly   Dictionary<(nint, nint), WgpuBindGroup>    bindGroup1 = new ();
+
+        protected override void Clear() {
+            ReleaseBindGroups(bindGroup0);
+            ReleaseBindGroups(bindGroup1);
+        }
+    }
+
+    private static readonly int _Textures_GPU_ShaderId            =  ShaderRegistry.NewShaderId("Textures");
+    private const  ulong        _Textures_GPU_layout_0_Key        =  0x4e47cc6a8a53afab;
+    private const  ulong        _Textures_GPU_layout_1_Key        =  0x1c264884083a5675;
+
+    private static ulong        _Textures_GPU_WgslHash            => 0x6daa93cb8c2d50ccUL;  // support Hot-Reload
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static ref readonly PipelineCache _Textures_GPU_CreatePipelineCache(WgpuDevice device, RenderConfig config)
+    {
+        Span<WgpuBindGroupLayout> layouts = stackalloc WgpuBindGroupLayout[2];
+        var layout_0 = device.GetBindGroupLayout(_Textures_GPU_layout_0_Key);
+        if (!layout_0.IsCreated) {
+            device.BindGroupLayoutTexture(TextureSampleType.Float, TextureViewDimension.D1D, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Float, TextureViewDimension.D2D, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Sint, TextureViewDimension.D2DArray, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Sint, TextureViewDimension.D3D, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Uint, TextureViewDimension.Cube, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Uint, TextureViewDimension.CubeArray, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Sint, TextureViewDimension.D2D, true);
+            device.BindGroupLayoutStorageTexture(TextureFormat.RGBA32Float, StorageTextureAccess.WriteOnly, TextureViewDimension.D1D);
+            device.BindGroupLayoutStorageTexture(TextureFormat.RGBA8Unorm, StorageTextureAccess.WriteOnly, TextureViewDimension.D2D);
+            device.BindGroupLayoutStorageTexture(TextureFormat.RGBA8Uint, StorageTextureAccess.WriteOnly, TextureViewDimension.D2DArray);
+            device.BindGroupLayoutStorageTexture(TextureFormat.R32Float, StorageTextureAccess.WriteOnly, TextureViewDimension.D3D);
+            device.BindGroupLayoutTexture(TextureSampleType.Depth, TextureViewDimension.D2D, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Depth, TextureViewDimension.D2DArray, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Depth, TextureViewDimension.Cube, false);
+            device.BindGroupLayoutTexture(TextureSampleType.Depth, TextureViewDimension.CubeArray, false);
+            layout_0 = device.CreateBindGroupLayout(ShaderStage.Vertex | ShaderStage.Fragment, _Textures_GPU_layout_0_Key, "Textures_layout_0"u8);
+        }
+        layouts[0] = layout_0;
+        
+        var layout_1 = device.GetBindGroupLayout(_Textures_GPU_layout_1_Key);
+        if (!layout_1.IsCreated) {
+            device.BindGroupLayoutSampler(SamplerBindingType.Filtering);
+            device.BindGroupLayoutSampler(SamplerBindingType.Comparison);
+            layout_1 = device.CreateBindGroupLayout(ShaderStage.Vertex | ShaderStage.Fragment, _Textures_GPU_layout_1_Key, "Textures_layout_1"u8);
+        }
+        layouts[1] = layout_1;
+        
+        var pipeline = device.CreateRenderPipeline(layouts, config, typeof(ShaderExample), _Textures_GPU_Shaders, "Textures_pipeline"u8);
+
+        var bindGroupCache = new _Textures_GPU_Cache();
+        return ref device.CreatePipelineCache(_Textures_GPU_ShaderId, config, _Textures_GPU_WgslHash, pipeline, layouts, bindGroupCache);
+    }
+    
+    private static readonly WgpuShader[] _Textures_GPU_Shaders = [
+        new WgpuShader("shaders/testTextureTypes.frag.wgsl"),
+    ];
+
+}
