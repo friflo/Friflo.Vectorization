cf555994de599389c83daecc62b302eabeadf76e
friflo <ullrich@praetz.de>
2026-07-23 09:17:16 +0200

    Transpiler - order CsTypeCode values by primitive type

--- src/Friflo.WGSL.Transpiler/CSharp/CsTypes.cs
+++ src/Friflo.WGSL.Transpiler/CSharp/CsTypes.cs
@@ -42,29 +42,30 @@ public static class CsExtensions
             return default;
         } 
     }
-
-     
 }
-
 
 
 public enum CsTypeCode
 {
     None,
     // --- WGSL Types
-    f16, f32,
-    i32, u32,
+    f16, f32, i32, u32,
     
-    vec2h, vec3h, vec4h,
-    vec2f, vec3f, vec4f,
-    vec2i, vec3i, vec4i,
-    vec2u, vec3u, vec4u,
+    vec2h, vec2f, vec2i, vec2u,
+    vec3h, vec3f, vec3i, vec3u,
+    vec4h, vec4f, vec4i, vec4u,
     
-    mat2x3h, mat2x4h, mat3x2h, mat3x4h, mat4x2h, mat4x3h,
-    mat2x3f, mat2x4f, mat3x2f, mat3x4f, mat4x2f, mat4x3f,
-    
-    mat2x2h, mat3x3h, mat4x4h,
-    mat2x2f, mat3x3f, mat4x4f,
+    mat2x2h, mat2x2f,
+    mat2x3h, mat2x3f,
+    mat2x4h, mat2x4f,
+
+    mat3x2h, mat3x2f,
+    mat3x3h, mat3x3f,
+    mat3x4h, mat3x4f,
+
+    mat4x2h, mat4x2f,
+    mat4x3h, mat4x3f,
+    mat4x4h, mat4x4f,
     
     WgslStruct,     // Last WGSL Type
     
