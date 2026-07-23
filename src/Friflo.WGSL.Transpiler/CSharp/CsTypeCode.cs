c8112ee005c56bbaac51f185d4e1f7401ab79c39
friflo <ullrich@praetz.de>
2026-07-23 16:12:49 +0200

    Transpiler - add extension property CsTypeCode.ByteSize

--- src/Friflo.WGSL.Transpiler/CSharp/CsTypeCode.cs
+++ src/Friflo.WGSL.Transpiler/CSharp/CsTypeCode.cs
@@ -63,7 +63,12 @@ public static class CsExtensions
     {
         public bool IsBuffer   => typeCode is CsTypeCode.InBuffer or CsTypeCode.InOutBuffer; 
     }
-
+    
+    extension (CsTypeCode typeCode)
+    {
+        public int ByteSize => TypeSizes[(int)typeCode];
+    }
+    
     extension (ValueArray<CsTypeInfo> typeInfos)
     {
         public CsTypeInfo FindTypeInfo(string @namespace, string name)
@@ -76,5 +81,77 @@ public static class CsExtensions
             return default;
         } 
     }
+    
+    private static readonly int[] TypeSizes;
+    
+    private static void SetSize(int[] typeSizes, CsTypeCode code, int sizeInBytes)
+    {
+        typeSizes[(int)code] = sizeInBytes;
+    } 
+        
+    static CsExtensions()
+    {
+        const int length = (int)CsTypeCode.WgslStruct;
+        var sizes = TypeSizes = new int [length];
+        
+        // --- Scalars
+        SetSize(sizes, CsTypeCode.f16, 2);
+        SetSize(sizes, CsTypeCode.f32, 4);
+        SetSize(sizes, CsTypeCode.i32, 4);
+        SetSize(sizes, CsTypeCode.u32, 4);
+
+        
+        // --- 2-Component Vectors
+        SetSize(sizes, CsTypeCode.vec2h, 4);   // 2x f16
+        SetSize(sizes, CsTypeCode.vec2f, 8);   // 2x f32
+        SetSize(sizes, CsTypeCode.vec2i, 8);   // 2x i32
+        SetSize(sizes, CsTypeCode.vec2u, 8);   // 2x u32
+
+        // --- 3-Component Vectors (Payload size, buffer alignment is padded)
+        SetSize(sizes, CsTypeCode.vec3h, 6);   // 3x f16 (8 bytes in buffer)
+        SetSize(sizes, CsTypeCode.vec3f, 12);  // 3x f32 (16 bytes in buffer)
+        SetSize(sizes, CsTypeCode.vec3i, 12);  // 3x i32 (16 bytes in buffer)
+        SetSize(sizes, CsTypeCode.vec3u, 12);  // 3x u32 (16 bytes in buffer)
+
+        // --- 4-Component Vectors
+        SetSize(sizes, CsTypeCode.vec4h, 8);   // 4x f16
+        SetSize(sizes, CsTypeCode.vec4f, 16);  // 4x f32
+        SetSize(sizes, CsTypeCode.vec4i, 16);  // 4x i32
+        SetSize(sizes, CsTypeCode.vec4u, 16);  // 4x u32
+
+        
+        // --- 2xN Matrices (Columns x Rows)
+        SetSize(sizes, CsTypeCode.mat2x2h, 8);   // 2x vec2h
+        SetSize(sizes, CsTypeCode.mat2x2f, 16);  // 2x vec2f
+
+        SetSize(sizes, CsTypeCode.mat2x3h, 12);  // 2x vec3h (16 bytes in buffer)
+        SetSize(sizes, CsTypeCode.mat2x3f, 24);  // 2x vec3f (32 bytes in buffer)
+
+        SetSize(sizes, CsTypeCode.mat2x4h, 16);  // 2x vec4h
+        SetSize(sizes, CsTypeCode.mat2x4f, 32);  // 2x vec4f
+
+        // --- 3xN Matrices
+        SetSize(sizes, CsTypeCode.mat3x2h, 12);  // 3x vec2h
+        SetSize(sizes, CsTypeCode.mat3x2f, 24);  // 3x vec2f
+
+        SetSize(sizes, CsTypeCode.mat3x3h, 18);  // 3x vec3h (24 bytes in buffer)
+        SetSize(sizes, CsTypeCode.mat3x3f, 36);  // 3x vec3f (48 bytes in buffer)
+
+        SetSize(sizes, CsTypeCode.mat3x4h, 24);  // 3x vec4h
+        SetSize(sizes, CsTypeCode.mat3x4f, 48);  // 3x vec4f
+
+        // --- 4xN Matrices
+        SetSize(sizes, CsTypeCode.mat4x2h, 16);  // 4x vec2h
+        SetSize(sizes, CsTypeCode.mat4x2f, 32);  // 4x vec2f
+
+        SetSize(sizes, CsTypeCode.mat4x3h, 24);  // 4x vec3h (32 bytes in buffer)
+        SetSize(sizes, CsTypeCode.mat4x3f, 48);  // 4x vec3f (64 bytes in buffer)
+
+        SetSize(sizes, CsTypeCode.mat4x4h, 32);  // 4x vec4h
+        SetSize(sizes, CsTypeCode.mat4x4f, 64);  // 4x vec4f
+    }
+    
+
+
 }
 
