3965f8ffb9ec0d65eef9534eaa68eeb08002504e
friflo <ullrich@praetz.de>
2026-07-26 16:52:09 +0200

    Transpiler - validate WgslTypeMapping elements

--- src/Friflo.WGSL.Transpiler/WGSL/CSharpType.cs
+++ src/Friflo.WGSL.Transpiler/WGSL/CSharpType.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using Friflo.WGSL.Transpiler.CSharp;
 
 // ReSharper disable InconsistentNaming
@@ -77,7 +78,6 @@ public struct WgslTypeMapping
 {
     public string   wgsl    { get; set; }
     public string   type    { get; set; }
-    public string   ns      { get; set; }
     
     public static WgslType2CSharpType[] LoadTypeMapping(string path, out string error)
     {
@@ -94,9 +94,25 @@ public struct WgslTypeMapping
             foreach (var mapping in mappings)
             {
                 if (!Enum.TryParse<CsTypeCode>(mapping.wgsl, out var typeCode)) {
-                    continue;
+                    error = $"Invalid wgsl type (non generic version required) type: ${mapping.wgsl}";
+                    return [];
                 }
-                list.Add(new WgslType2CSharpType(typeCode, mapping.ns, mapping.type));
+                var type = mapping.type;
+                var lastDot = type.LastIndexOf('.');
+                var className = type.Substring(lastDot + 1);
+                if (!IsValidCSharpIdentifier(className)) {
+                    error = $"Invalid C# type: ${type}";
+                    return [];
+                }
+                var @namespace = "";
+                if (lastDot != -1) {
+                    @namespace = type.Substring(0, lastDot);
+                    if (!IsValidCSharpIdentifier(@namespace)) {
+                        error = $"Invalid C# type: ${@type}";
+                        return [];
+                    }
+                }
+                list.Add(new WgslType2CSharpType(typeCode, @namespace, className));
             }
             error = null;
             return list.ToArray();
@@ -107,6 +123,11 @@ public struct WgslTypeMapping
             error = $"Loading {path} failed: {message}";
             return [];
         }
+    }
+    
+    private static bool IsValidCSharpIdentifier(string name)
+    {
+        return Regex.IsMatch(name, @"^[a-zA-Z_][a-zA-Z0-9_]*$");
     }
 }
 
