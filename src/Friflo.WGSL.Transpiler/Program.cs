9f965c1dc5190d36e3db081cbfa9107fc8e877b1
friflo <ullrich@praetz.de>
2026-07-30 09:46:08 +0200

    console - add transpiler as a prebuild step

---
+++ src/Friflo.WGSL.Transpiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Friflo.WGSL.Transpiler;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        // for (int n = 0; n < 3; n++) { Console.WriteLine("WGSL : error : ----- test log is shown -----"); }
+        return 0; // use 111 to see if Transpiler.dll is found and Main() is executed 
+    }
+}
