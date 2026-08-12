08b49ea2e1b13d1dd79b3b653fbedfb8fcdeea48
friflo <ullrich@praetz.de>
2026-08-12 12:22:53 +0200

    ImDraw - moved Draw2D to separate file

--- src/Friflo.WGPU.ImDraw/API/Batcher2D.cs
+++ src/Friflo.WGPU.ImDraw/API/Batcher2D.cs
@@ -3,11 +3,10 @@
 
 
 using System;
-using System.Numerics;
-using System.Runtime.CompilerServices;
 using Friflo.Vectorization.GPU;
 using Friflo.Vectorization.WebGPU;
 using Shaders.Imdraw;
+
 
 // ReSharper disable SuggestVarOrType_BuiltInTypes
 // ReSharper disable ConvertToPrimaryConstructor
@@ -111,92 +110,3 @@ public sealed partial class Batcher2D : IDisposable
     // [ ]  If needed, add parameter: [IndexBuffer] InBuffer<ushort|uint> indices.
 }
 
-
-
-public readonly ref struct Draw2D : IDisposable
-{
-    private readonly Batcher2D      batcher;
-    private readonly RenderPass     pass;   // RenderPass is ref struct
-
-    
-    public void Dispose() {
-        Flush();
-    }
-    
-    public Draw2D(Batcher2D batcherBatcher, RenderPass pass)
-    {
-        batcher     = batcherBatcher;
-        this.pass   = pass;
-    }
-    
-    public void Rectangle(in Vector2 position, in Vector2 size, uint color)
-    {
-        DrawQuad(position, size, color);
-    }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void DrawQuad(in Vector2 position, in Vector2 size, uint color, GpuTextureView? texture = null)
-    {
-        texture ??= batcher.defaultWhiteTextureView;
-        
-        if (batcher.vertexCount + 6 > batcher.vertexBuffer.Length || (batcher.currentTextureView != null && batcher.currentTextureView.Value.Handle != texture.Value.Handle)) {
-            Flush();
-        }
-        batcher.currentTextureView = texture;
-
-        var span = batcher.vertexBuffer.InOut(batcher.vertexCount, 6).Span;
-        
-        // fill Quad with two triangles (TL, TR, BL / TR, BR, BL)
-        FillQuad(span, position, size, color);
-
-        batcher.vertexCount += 6;
-    }
-    
-    /// <summary>
-    /// Fills a 6-vertex span representing two triangles for a quad (TL, TR, BL, TR, BR, BL).
-    /// </summary>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static void FillQuad(Span<Vertex2D> span, in Vector2 position, in Vector2 size, uint color)
-    {
-        float x1 = position.X;
-        float y1 = position.Y;
-        float x2 = position.X + size.X;
-        float y2 = position.Y + size.Y;
-
-        // V0: Top-Left
-        span[0] = new Vertex2D { position = new Vector2(x1, y1), uv = new Vector2(0f, 0f), color = color };
-        // V1: Top-Right
-        span[1] = new Vertex2D { position = new Vector2(x2, y1), uv = new Vector2(1f, 0f), color = color };
-        // V2: Bottom-Left
-        span[2] = new Vertex2D { position = new Vector2(x1, y2), uv = new Vector2(0f, 1f), color = color };
-
-        // V3: Top-Right (reused)
-        span[3] = span[1];
-        // V4: Bottom-Right
-        span[4] = new Vertex2D { position = new Vector2(x2, y2), uv = new Vector2(1f, 1f), color = color };
-        // V5: Bottom-Left (reused)
-        span[5] = span[2];
-    }
-    
-    public void SetViewport(float width, float height)
-    {
-        if (batcher.vertexCount > 0) {
-            Flush();
-        }
-        var proj = Matrix4x4.CreateOrthographicOffCenter(0f, width, height, 0f, -1f, 1f);
-        batcher.uniforms = new ImUniforms { projection = proj };
-    }
-
-    public void Flush()
-    {
-        if (batcher.vertexCount == 0) return;
-
-        batcher.vertexBuffer.InOut(0, batcher.vertexCount).Write();
-
-        var texture = batcher.currentTextureView ?? batcher.defaultWhiteTextureView;
-        
-        Batcher2D.Draw(pass, batcher.config, batcher.uniforms, texture, batcher.defaultSampler, batcher.vertexBuffer.In(0, batcher.vertexCount));
-        
-        batcher.vertexCount = 0;
-    }
-}
--- src/Friflo.WGPU.ImDraw/API/Draw2D.cs
+++ src/Friflo.WGPU.ImDraw/API/Draw2D.cs
@@ -5,31 +5,100 @@
 using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using Friflo.Vectorization.WebGPU;
+using Shaders.Imdraw;
 
-
+// ReSharper disable ConvertToPrimaryConstructor
 // ReSharper disable once CheckNamespace
 namespace Friflo.WGPU.ImDraw;
 
 
-
-
-/*
-public static class Draw2D
+public readonly ref struct Draw2D : IDisposable
 {
-    [ThreadStatic]
-    private static Batcher2D? _currentBatcher;
+    private readonly Batcher2D      batcher;
+    private readonly RenderPass     pass;   // RenderPass is ref struct
 
-    public static void Begin(Batcher2D batcher) => _currentBatcher = batcher;
-
+    
+    public void Dispose() {
+        Flush();
+    }
+    
+    public Draw2D(Batcher2D batcherBatcher, RenderPass pass)
+    {
+        batcher     = batcherBatcher;
+        this.pass   = pass;
+    }
+    
+    public void Rectangle(in Vector2 position, in Vector2 size, uint color)
+    {
+        DrawQuad(position, size, color);
+    }
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Rectangle(in Vector2 position, in Vector2 size, uint color)
+    public void DrawQuad(in Vector2 position, in Vector2 size, uint color, GpuTextureView? texture = null)
     {
-        _currentBatcher!.DrawQuad(position, size, color);
+        var bat = batcher;
+        texture ??= bat.defaultWhiteTextureView;
+        
+        if (bat.vertexCount + 6 > bat.vertexBuffer.Length || (bat.currentTextureView != null && bat.currentTextureView.Value.Handle != texture.Value.Handle)) {
+            Flush();
+        }
+        bat.currentTextureView = texture;
+
+        var span = bat.vertexBuffer.InOut(bat.vertexCount, 6).Span;
+        
+        // fill Quad with two triangles (TL, TR, BL / TR, BR, BL)
+        FillQuad(span, position, size, color);
+
+        bat.vertexCount += 6;
+    }
+    
+    /// <summary>
+    /// Fills a 6-vertex span representing two triangles for a quad (TL, TR, BL, TR, BR, BL).
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void FillQuad(Span<Vertex2D> span, in Vector2 position, in Vector2 size, uint color)
+    {
+        float x1 = position.X;
+        float y1 = position.Y;
+        float x2 = position.X + size.X;
+        float y2 = position.Y + size.Y;
+
+        // V0: Top-Left
+        span[0] = new Vertex2D { position = new Vector2(x1, y1), uv = new Vector2(0f, 0f), color = color };
+        // V1: Top-Right
+        span[1] = new Vertex2D { position = new Vector2(x2, y1), uv = new Vector2(1f, 0f), color = color };
+        // V2: Bottom-Left
+        span[2] = new Vertex2D { position = new Vector2(x1, y2), uv = new Vector2(0f, 1f), color = color };
+
+        // V3: Top-Right (reused)
+        span[3] = span[1];
+        // V4: Bottom-Right
+        span[4] = new Vertex2D { position = new Vector2(x2, y2), uv = new Vector2(1f, 1f), color = color };
+        // V5: Bottom-Left (reused)
+        span[5] = span[2];
+    }
+    
+    public void SetViewport(float width, float height)
+    {
+        if (batcher.vertexCount > 0) {
+            Flush();
+        }
+        var proj = Matrix4x4.CreateOrthographicOffCenter(0f, width, height, 0f, -1f, 1f);
+        batcher.uniforms = new ImUniforms { projection = proj };
     }
 
-    public static void End()
+    public void Flush()
     {
-        _currentBatcher?.Flush();
-        _currentBatcher = null;
+        var bat = batcher;
+        if (bat.vertexCount == 0) return;
+
+        bat.vertexBuffer.InOut(0, bat.vertexCount).Write();
+
+        var texture = bat.currentTextureView ?? bat.defaultWhiteTextureView;
+        
+        Batcher2D.Draw(pass, bat.config, bat.uniforms, texture, bat.defaultSampler, bat.vertexBuffer.In(0, bat.vertexCount));
+        
+        bat.vertexCount = 0;
     }
-} */
+}
