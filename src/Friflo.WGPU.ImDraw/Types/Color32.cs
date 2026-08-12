6bd5d1d8af30ba76da1af36ef5a98a3714e09156
friflo <ullrich@praetz.de>
2026-08-12 12:49:21 +0200

    ImDraw - add Color32

---
+++ src/Friflo.WGPU.ImDraw/Types/Color32.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ConvertToPrimaryConstructor
+// ReSharper disable UnusedMember.Global
+// ReSharper disable once CheckNamespace
+namespace Friflo.WGPU.ImDraw;
+
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public readonly struct Color32
+{
+    public readonly uint Value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Color32(byte r, byte g, byte b, byte a = 255)
+    {
+        Value = (uint)r | ((uint)g << 8) | ((uint)b << 16) | ((uint)a << 24);
+    }
+
+    // Direct implicit cast to uint -> zero performance penalty at callsites
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator uint(Color32 color) => color.Value;
+
+    // Presets
+    // --- Special ---
+    public static Color32 Transparent      => new(0, 0, 0, 0);
+    
+    // --- Basic Grayscale ---
+    public static Color32 Black            => new(0, 0, 0);
+    public static Color32 DarkGray         => new(64, 64, 64);
+    public static Color32 Gray             => new(128, 128, 128);
+    public static Color32 LightGray        => new(192, 192, 192);
+    public static Color32 White            => new(255, 255, 255);
+    
+    // --- Primaries & Secondaries ---
+    public static Color32 Red              => new(255, 0, 0);
+    public static Color32 Green            => new(0, 255, 0);
+    public static Color32 Blue             => new(0, 0, 255);
+    public static Color32 Yellow           => new(255, 255, 0);
+    public static Color32 Cyan             => new(0, 255, 255);
+    public static Color32 Magenta          => new(255, 0, 255);
+    
+    // --- Game Dev & UI Classics ---
+    public static Color32 Orange           => new(255, 165, 0);
+    public static Color32 Lime             => new(50, 205, 50);
+    public static Color32 CornflowerBlue   => new(100, 149, 237); // XNA/MonoGame
+    public static Color32 Purple           => new(128, 0, 128);
+    public static Color32 Pink             => new(255, 192, 203);
+    public static Color32 Gold             => new(255, 215, 0);
+    public static Color32 Teal             => new(0, 128, 128);
+    public static Color32 Navy             => new(0, 0, 128);
+    public static Color32 Crimson          => new(220, 20, 60);
+}
--- src/Tests-Console/ImDraw2DTest.cs
+++ src/Tests-Console/ImDraw2DTest.cs
@@ -44,9 +44,9 @@ public class ImRenderer : IRenderer
         draw.Rectangle(new Vector2(1, 1), new Vector2(40, 40), 0xFFFFFFFF);
         draw.Rectangle(new Vector2(frame.Width - 41, frame.Height - 41), new Vector2(40, 40), 0xFFFFFFFF);
         
-        draw.Rectangle(new Vector2(100, 50), new Vector2(100, 100), 0xFF0000FF);
-        draw.Rectangle(new Vector2(300, 50), new Vector2(100, 100), 0xFF00FF00);
-        draw.Rectangle(new Vector2(500, 50), new Vector2(100, 100), 0xFFFF0000);
+        draw.Rectangle(new Vector2(100, 50), new Vector2(100, 100), Color32.Red);
+        draw.Rectangle(new Vector2(300, 50), new Vector2(100, 100), Color32.Green);
+        draw.Rectangle(new Vector2(500, 50), new Vector2(100, 100), Color32.Blue);
         
         draw.Flush(); // redundant call
     }
