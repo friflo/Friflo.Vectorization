8996fe6109634437fd1b87005b40910c6045f68d
friflo <ullrich@praetz.de>
2026-08-26 17:33:39 +0200

    wgppu - moved WgpuGuiExtensions to wgpu

---
+++ src/Friflo.WGPU/ImGui/WgpuGuiExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Friflo.ImGui;
+
+
+// ReSharper disable ConvertToPrimaryConstructor
+// ReSharper disable CheckNamespace
+namespace Friflo.WGPU.ImGui;
+
+public static class WgpuGuiExtensions
+{
+    public static ImTexture ToImTexture(this GpuTextureView view)
+    {
+        return new ImTexture(view.texture, view.Handle);
+    }
+}
