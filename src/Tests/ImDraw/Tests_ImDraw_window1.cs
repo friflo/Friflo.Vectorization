b4f2139d957f55fa678ff4d50cb2d925cf32e26e
friflo <ullrich@praetz.de>
2026-08-27 07:34:34 +0200

    Tests - add Tests_ImDraw_window1 skeleton

---
+++ src/Tests/ImDraw/Tests_ImDraw_window1.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using Friflo.ImGui;
+
+
+// ReSharper disable ArrangeObjectCreationWhenTypeNotEvident
+// ReSharper disable once InconsistentNaming
+namespace Tests.ImDraw;
+
+public class Tests_ImDraw_window1
+{
+    private bool    mouseCircle;
+    private bool    monocraft;
+    private bool    enabled2;
+    private float   volume;
+    
+    // [Test]
+    public void Tests_ImDraw_window1_gui(Batch2D batch)
+    {
+        var gui = batch.BeginGui(1280, 1000);
+        
+        using (gui.BeginWindow("Window 1", new(100, 20), new(400, 950))) {
+            Window1(gui); 
+        }
+    }
+    
+    private readonly GuiStyle redButtonStyle = new() {
+        color = new GuiColor {
+            ButtonColor = 0xaa4444ff,
+            ButtonHover = 0xcc6666ff,
+            ButtonDown  = 0x882222ff,
+            ButtonText  = 0xffffffff,
+            FocusColor  = 0xffffffff,
+        }
+    };
+    
+    private readonly GuiStyle greenButtonStyle = new() {
+        color = new GuiColor {
+            ButtonColor = 0x229922ff,
+            ButtonHover = 0x44bb44ff,
+            ButtonDown  = 0x007700ff,
+            ButtonText  = 0xffffffff,
+            FocusColor  = 0xffffffff,
+        }
+    };
+    
+    private void Window1(Gui gui)
+    {
+        gui.Label("hello GUI");
+        gui.Spacer();
+        using (gui.PushStyle(greenButtonStyle)) {
+            if (gui.Button("hello"))                            Console.WriteLine("Clicked: hello");
+        }
+
+        gui.Spacer();
+        gui.Checkbox("mouse circle", ref mouseCircle);
+        if(gui.Checkbox("Monocraft", ref monocraft)) {
+        }
+        gui.Spacer();
+        if (gui.Slider("Volume", ref volume, 0f, 1f, 200)) Console.WriteLine($"Volume: changed");
+        gui.Spacer();
+        
+        gui.BeginHorizontal();
+            if (gui.Button("First"))                            Console.WriteLine("Clicked: First");
+            gui.Spacer(10);
+            if (gui.Button("Second"))                           Console.WriteLine("Clicked: Second");
+            gui.Spacer(10);
+            if (gui.Button("Red", redButtonStyle))              Console.WriteLine("Clicked: Red");
+        gui.EndHorizontal();
+        
+        gui.Label("after horizontal");
+        using (var space = gui.BeginSpace(new(64, 64), "sprite")) {
+            if (space.isFired) Console.WriteLine("Clicked: Sprite");
+        }
+        gui.Spacer();
+        gui.Checkbox("checkbox", ref enabled2);
+        
+        gui.Spacer();
+        
+        using (gui.BeginHorizontalAligned(47, HorizontalAlignment.Right)) {
+            gui.Button("Right");
+            gui.Button(" A´");
+            gui.Button(" B´");
+            gui.Button(" C´ ");
+        }
+        using (gui.BeginHorizontalAligned(11, HorizontalAlignment.Center)) {
+            gui.Button("Center");
+            gui.Button(" 1 ");
+            gui.Button(" 2 ");
+            gui.Button(" 3 ");
+        }
+        using (gui.BeginHorizontal()) {
+            gui.Button("usual");
+            gui.Button("horizontal");
+            gui.Button(" A ");
+            gui.Button(" B ");
+            gui.Button(" C ");
+        }
+    }
+}
