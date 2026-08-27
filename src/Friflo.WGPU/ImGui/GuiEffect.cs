9b3d3e702b10f275d73fc9fc5a0474077770f84c
friflo <ullrich@praetz.de>
2026-08-27 06:26:37 +0200

    wgpu - removed unused import

--- src/Friflo.WGPU/ImGui/GuiEffect.cs
+++ src/Friflo.WGPU/ImGui/GuiEffect.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using System.Runtime.CompilerServices;
 using Friflo.ImGui;
 
 // ReSharper disable CheckNamespace
