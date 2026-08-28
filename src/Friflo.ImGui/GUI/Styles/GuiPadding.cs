0aaab1e8145860188955f1cdfe670cc791134f16
friflo <ullrich@praetz.de>
2026-08-28 16:50:56 +0200

    ImGui - add GuiPadding

---
+++ src/Friflo.ImGui/GUI/Styles/GuiPadding.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+using static System.Diagnostics.DebuggerBrowsableState;
+using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
+
+// ReSharper disable once CheckNamespace
+namespace Friflo.ImGui;
+
+
+public enum PaddingId
+{
+    WindowPadding,
+    ButtonPadding,
+    SliderPadding,
+    CellPadding,
+    ContainerPadding
+}
+
+
+
+public struct GuiPadding
+{
+    public Padding2D WindowPadding    { readonly get => windowPadding;    set => windowPadding    = Add(PaddingId.WindowPadding,    value); }
+    public Padding2D ButtonPadding    { readonly get => buttonPadding;    set => buttonPadding    = Add(PaddingId.ButtonPadding,    value); }
+    public Padding2D SliderPadding    { readonly get => sliderPadding;    set => sliderPadding    = Add(PaddingId.SliderPadding,    value); }
+    public Padding2D CellPadding      { readonly get => cellPadding;      set => cellPadding      = Add(PaddingId.CellPadding,      value); }
+    public Padding2D ContainerPadding { readonly get => containerPadding; set => containerPadding = Add(PaddingId.ContainerPadding, value); }
+
+    public readonly             Bitset64<PaddingId> Overrides                       => overrides;
+    public readonly override    string              ToString()                      => $"overrides: {overrides.Count}";
+
+    public                      void                RemoveOverride(PaddingId id)    => overrides.Remove(id);
+    public readonly             bool                HasOverride(PaddingId id)       => overrides.Contains(id);
+    public                      void                ClearOverrides()                => overrides = default;
+
+#region internal
+    [Browse(Never)] private     Padding2D   windowPadding;
+    [Browse(Never)] private     Padding2D   buttonPadding;
+    [Browse(Never)] private     Padding2D   sliderPadding;
+    [Browse(Never)] private     Padding2D   cellPadding;
+    [Browse(Never)] private     Padding2D   containerPadding;
+
+    [Browse(Never)] internal Bitset64<PaddingId> overrides;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private Padding2D Add(PaddingId id, Padding2D padding) 
+    {
+        overrides.value |= 1UL << Unsafe.As<PaddingId, int>(ref id);
+        return padding;
+    }
+
+    internal static void ApplyOverrides(in GuiPadding source, ref GuiPadding target, Bitset64<PaddingId> overrides)
+    {
+        foreach (var id in overrides)
+        {
+            switch (id) {
+                case PaddingId.WindowPadding:    target.windowPadding    = source.windowPadding;    break;
+                case PaddingId.ButtonPadding:    target.buttonPadding    = source.buttonPadding;    break;
+                case PaddingId.SliderPadding:    target.sliderPadding    = source.sliderPadding;    break;
+                case PaddingId.CellPadding:      target.cellPadding      = source.cellPadding;      break;
+                case PaddingId.ContainerPadding: target.containerPadding = source.containerPadding; break;
+            }
+        }
+    }
+#endregion
+
+    public void AddOverrides(in GuiPadding source)
+    {
+        foreach (var id in source.overrides)
+        {
+            switch (id) {
+                case PaddingId.WindowPadding:    WindowPadding    = source.windowPadding;    break;
+                case PaddingId.ButtonPadding:    ButtonPadding    = source.buttonPadding;    break;
+                case PaddingId.SliderPadding:    SliderPadding    = source.sliderPadding;    break;
+                case PaddingId.CellPadding:      CellPadding      = source.cellPadding;      break;
+                case PaddingId.ContainerPadding: ContainerPadding = source.containerPadding; break;
+            }
+        }
+    }
+}
---
+++ src/Friflo.ImGui/GUI/Styles/Padding2D.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// ReSharper disable CompareOfFloatsByEqualityOperator
+// ReSharper disable once CheckNamespace
+namespace Friflo.ImGui;
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct Padding2D : IEquatable<Padding2D>
+{
+    // Min = (Left, Top), Max = (Right, Bottom)
+    public readonly     Vector2     Min;
+    public readonly     Vector2     Max;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Padding2D(float uniform)
+    {
+        Min = new Vector2(uniform);
+        Max = new Vector2(uniform);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Padding2D(float horizontal, float vertical)
+    {
+        Min = new Vector2(horizontal, vertical);
+        Max = new Vector2(horizontal, vertical);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Padding2D(Vector2 min, Vector2 max)
+    {
+        Min = min;
+        Max = max;
+    }
+
+    public  float   Left    => Min.X;
+    public  float   Top     => Min.Y;
+    public  float   Right   => Max.X;
+    public  float   Bottom  => Max.Y;
+
+    public  Vector2 Size    => Min + Max;
+
+    public static Padding2D Zero => default;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Expand(Vector2 size) => size + Size;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Shrink(Vector2 size) => size - Size;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Padding2D(float uniform) => new(uniform);
+
+    public          bool    Equals(Padding2D other) => Min == other.Min && Max == other.Max;
+    public override bool    Equals(object? obj)     => obj is Padding2D other && Equals(other);
+    public override int     GetHashCode()           => HashCode.Combine(Min, Max);
+}
