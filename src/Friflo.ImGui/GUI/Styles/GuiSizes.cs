bad70f2c51db09152f3d3715a0e2af618f89e255
friflo <ullrich@praetz.de>
2026-08-29 09:35:58 +0200

    ImGui - rename enum -> SizeId

--- src/Friflo.ImGui/GUI/Styles/GuiSizes.cs
+++ src/Friflo.ImGui/GUI/Styles/GuiSizes.cs
@@ -10,7 +10,7 @@ using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
 namespace Friflo.ImGui;
 
 
-public enum PaddingId
+public enum SizeId
 {
     WindowPadding,
     FramePadding,
@@ -23,21 +23,20 @@ public enum PaddingId
 
 public struct GuiSizes
 {
-    public float     CornerRadius => 8;
+    public float        CornerRadius => 8;
     
-    
-    public Padding2D    WindowPadding    { readonly get => windowPadding;    set => windowPadding    = Add(PaddingId.WindowPadding,    value); }
-    public Padding2D    FramePadding     { readonly get => framePadding;     set => framePadding     = Add(PaddingId.FramePadding,     value); }
-    public Vector2      ItemSpacing      { readonly get => itemSpacing;      set => itemSpacing      = Add(PaddingId.ItemSpacing,      value); }
-    public Padding2D    CellPadding      { readonly get => cellPadding;      set => cellPadding      = Add(PaddingId.CellPadding,      value); }
-    public Padding2D    ContainerPadding { readonly get => containerPadding; set => containerPadding = Add(PaddingId.ContainerPadding, value); }
+    public Padding2D    WindowPadding    { readonly get => windowPadding;    set => windowPadding    = Add(SizeId.WindowPadding,    value); }
+    public Padding2D    FramePadding     { readonly get => framePadding;     set => framePadding     = Add(SizeId.FramePadding,     value); }
+    public Vector2      ItemSpacing      { readonly get => itemSpacing;      set => itemSpacing      = Add(SizeId.ItemSpacing,      value); }
+    public Padding2D    CellPadding      { readonly get => cellPadding;      set => cellPadding      = Add(SizeId.CellPadding,      value); }
+    public Padding2D    ContainerPadding { readonly get => containerPadding; set => containerPadding = Add(SizeId.ContainerPadding, value); }
 
-    public readonly             Bitset64<PaddingId> Overrides                       => overrides;
-    public readonly override    string              ToString()                      => $"overrides: {overrides.Count}";
+    public readonly             Bitset64<SizeId>    Overrides                   => overrides;
+    public readonly override    string              ToString()                  => $"overrides: {overrides.Count}";
 
-    public                      void                RemoveOverride(PaddingId id)    => overrides.Remove(id);
-    public readonly             bool                HasOverride(PaddingId id)       => overrides.Contains(id);
-    public                      void                ClearOverrides()                => overrides = default;
+    public                      void                RemoveOverride(SizeId id)   => overrides.Remove(id);
+    public readonly             bool                HasOverride   (SizeId id)   => overrides.Contains(id);
+    public                      void                ClearOverrides()            => overrides = default;
 
 #region internal
     [Browse(Never)] private     Padding2D   windowPadding;
@@ -46,32 +45,32 @@ public struct GuiSizes
     [Browse(Never)] private     Padding2D   cellPadding;
     [Browse(Never)] private     Padding2D   containerPadding;
 
-    [Browse(Never)] internal Bitset64<PaddingId> overrides;
+    [Browse(Never)] internal Bitset64<SizeId> overrides;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private Padding2D Add(PaddingId id, Padding2D value) 
+    private Padding2D Add(SizeId id, Padding2D value) 
     {
-        overrides.value |= 1UL << Unsafe.As<PaddingId, int>(ref id);
+        overrides.value |= 1UL << Unsafe.As<SizeId, int>(ref id);
         return value;
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private Vector2 Add(PaddingId id, Vector2 value) 
+    private Vector2 Add(SizeId id, Vector2 value) 
     {
-        overrides.value |= 1UL << Unsafe.As<PaddingId, int>(ref id);
+        overrides.value |= 1UL << Unsafe.As<SizeId, int>(ref id);
         return value;
     }
 
-    internal static void ApplyOverrides(in GuiSizes source, ref GuiSizes target, Bitset64<PaddingId> overrides)
+    internal static void ApplyOverrides(in GuiSizes source, ref GuiSizes target, Bitset64<SizeId> overrides)
     {
         foreach (var id in overrides)
         {
             switch (id) {
-                case PaddingId.WindowPadding:       target.windowPadding    = source.windowPadding;     break;
-                case PaddingId.FramePadding:        target.framePadding     = source.framePadding;      break;
-                case PaddingId.ItemSpacing:         target.itemSpacing      = source.itemSpacing;       break;
-                case PaddingId.CellPadding:         target.cellPadding      = source.cellPadding;       break;
-                case PaddingId.ContainerPadding:    target.containerPadding = source.containerPadding;  break;
+                case SizeId.WindowPadding:      target.windowPadding    = source.windowPadding;     break;
+                case SizeId.FramePadding:       target.framePadding     = source.framePadding;      break;
+                case SizeId.ItemSpacing:        target.itemSpacing      = source.itemSpacing;       break;
+                case SizeId.CellPadding:        target.cellPadding      = source.cellPadding;       break;
+                case SizeId.ContainerPadding:   target.containerPadding = source.containerPadding;  break;
             }
         }
     }
@@ -82,11 +81,11 @@ public struct GuiSizes
         foreach (var id in source.overrides)
         {
             switch (id) {
-                case PaddingId.WindowPadding:       WindowPadding       = source.windowPadding;     break;
-                case PaddingId.FramePadding:        FramePadding        = source.framePadding;      break;
-                case PaddingId.ItemSpacing:         ItemSpacing         = source.itemSpacing;       break;
-                case PaddingId.CellPadding:         CellPadding         = source.cellPadding;       break;
-                case PaddingId.ContainerPadding:    ContainerPadding    = source.containerPadding;  break;
+                case SizeId.WindowPadding:      WindowPadding       = source.windowPadding;     break;
+                case SizeId.FramePadding:       FramePadding        = source.framePadding;      break;
+                case SizeId.ItemSpacing:        ItemSpacing         = source.itemSpacing;       break;
+                case SizeId.CellPadding:        CellPadding         = source.cellPadding;       break;
+                case SizeId.ContainerPadding:   ContainerPadding    = source.containerPadding;  break;
             }
         }
     }
