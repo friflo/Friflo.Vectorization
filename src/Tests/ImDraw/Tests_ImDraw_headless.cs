4a9c566d85ac0029e8aa3172aed3061d19846894
friflo <ullrich@praetz.de>
2026-08-30 17:28:56 +0200

    Tests - update ImGui profile stats

--- src/Tests/ImDraw/Tests_ImDraw_headless.cs
+++ src/Tests/ImDraw/Tests_ImDraw_headless.cs
@@ -33,7 +33,7 @@ public class Tests_ImDraw_headless
         var         batch   = backend.CreateBatch();
         EnsureBatchApi(batch);
         long        start = 0;
-        const int   repeat  = 10; // 100_000 - 0.98 sec    bottleneck: FillArc() - GuiSizes.CornerSegments = 4
+        const int   repeat  = 10; // 500_000 - 3.5 sec    bottleneck: FillArc() - GuiSizes.CornerSegments = 4
         
         for (int n = 0; n < repeat; n++)
         {
