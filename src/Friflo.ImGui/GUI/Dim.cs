f2c651424b8c89e5cf0fa3a2a44d8e2c124bbdf9
friflo <ullrich@praetz.de>
2026-08-30 07:54:53 +0200

    ImGui - Update Dim struct

--- src/Friflo.ImGui/GUI/Dim.cs
+++ src/Friflo.ImGui/GUI/Dim.cs
@@ -5,6 +5,8 @@
 using System.Diagnostics;
 using System.Numerics;
 
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
 // ReSharper disable ConvertToPrimaryConstructor
 // ReSharper disable once CheckNamespace
 namespace Friflo.ImGui;
@@ -13,8 +15,8 @@ namespace Friflo.ImGui;
 /// <summary>  Defines how a layout dimension is calculated. </summary>
 public enum Sizing : byte
 {
-    /** Explicit pixel size determined by the developer. */                 Exact,
     /** Size is determined by the inner content (children or text). */      Content,
+    /** Explicit pixel size determined by the developer. */                 Exact,
     /** Takes up the remaining available space in the parent container. */  Fill
 }
 
@@ -36,33 +38,51 @@ public readonly struct Dim
         this.sizingY    = sizingY;
     }
 
-    // --- Factory Methods ---
-
+#region Exact
     /// <summary>Creates explicit fixed pixel bounds for both axes.</summary>
     [DebuggerStepThrough]
-    public static Dim Exact(float width, float height)  => new(width,   Sizing.Exact,   height, Sizing.Exact);
+    public static Dim   Exact(float width, float height)    => new(width,   Sizing.Exact,   height, Sizing.Exact);
+    
+    /// <summary>Creates explicit fixed pixel bounds for a single axis while keeping the other at Content.</summary>
+    [DebuggerStepThrough]
+    public static Dim   ExactX(float width)     => new(width,   Sizing.Exact,   0f,     Sizing.Content);
 
+    [DebuggerStepThrough]
+    public static Dim   ExactY(float height)    => new(0f,      Sizing.Content, height, Sizing.Exact);
+#endregion  
+
+
+#region Fill
     /// <summary>Fills remaining horizontal space. Height is optionally fixed or defaults to Content.</summary>
     [DebuggerStepThrough]
-    public static Dim FillX(float height)               => new(0f,      Sizing.Fill,    height, Sizing.Exact);
+    public static Dim   FillX(float height)     => new(0f,      Sizing.Fill,    height, Sizing.Exact);
 
     [DebuggerStepThrough]
-    public static Dim FillX()                           => new(0f,      Sizing.Fill,    0f,     Sizing.Content);
+    public static Dim   FillX()                 => new(0f,      Sizing.Fill,    0f,     Sizing.Content);
 
     /// <summary>Fills remaining vertical space. Width is optionally fixed or defaults to Content.</summary>
     [DebuggerStepThrough]
-    public static Dim FillY(float width)                => new(width,   Sizing.Exact,   0f,     Sizing.Fill);
+    public static Dim   FillY(float width)      => new(width,   Sizing.Exact,   0f,     Sizing.Fill);
 
     [DebuggerStepThrough]
-    public static Dim FillY()                           => new(0f,      Sizing.Content, 0f,     Sizing.Fill);
+    public static Dim   FillY()                 => new(0f,      Sizing.Content, 0f,     Sizing.Fill);
 
     /// <summary>Fills remaining space in both directions.</summary>
     [DebuggerStepThrough]
-    public static Dim Fill()                            => new(0f,      Sizing.Fill,    0f,     Sizing.Fill);
+    public static Dim   Fill()                  => new(0f,      Sizing.Fill,    0f,     Sizing.Fill);
+    
+    /// <summary>Fills remaining horizontal space while explicitly specifying Y sizing mode.</summary>
+    [DebuggerStepThrough]
+    public static Dim   FillX(Sizing sizingY)   => new(0f,      Sizing.Fill,    0f,     sizingY);
+
+    /// <summary>Fills remaining vertical space while explicitly specifying X sizing mode.</summary>
+    [DebuggerStepThrough]
+    public static Dim   FillY(Sizing sizingX)   => new(0f,      sizingX,        0f,     Sizing.Fill);
+#endregion
 
     /// <summary>Sizes both axes according to inner content bounds.</summary>
     [DebuggerStepThrough]
-    public static Dim Content()                         => new(0f,      Sizing.Content, 0f,     Sizing.Content);
+    public static Dim   Content()               => new(0f,      Sizing.Content, 0f,     Sizing.Content);
 
     // --- Operator Conversions ---
 
