50a50a2f3ab46d939bb78927a50de67077b3be4d
friflo <ullrich@praetz.de>
2026-09-08 16:58:37 +0200

    terminal - format OpenSSH Proxy comment

--- src/Tests-TuiTerminalServer/Program.cs
+++ src/Tests-TuiTerminalServer/Program.cs
@@ -7,16 +7,16 @@ using TerminalServer;
 // connect terminal client
 // Windows plink(PuTTY)     plink -raw -t -P 9000 127.0.0.1       with args: echo --view logs --theme dark | plink -raw -t -P 9000 127.0.0.1
 // Windows WSL              stty raw -echo; nc $(ip route show default | awk '{print $3}') 9000; stty sane
-// macOS / Linux            stty raw -echo; nc localhost 9000; stty sane 
+// macOS / Linux            stty raw -echo; nc localhost 9000; stty sane
 
 // Or use an SSH Proxy to redirect SSH client connections to the TmGui TCP server:
-// Client (All OS):   ssh tmgui@127.0.0.1
+// Client (All OS):         ssh tmgui@127.0.0.1
 // Server Windows:
-//   - Setup:  Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0; Start-Service sshd
-//   - Config: Match User tmgui -> ForceCommand powershell -Command "$s=New-Object System.Net.Sockets.TcpClient('127.0.0.1',9000);$st=$s.GetStream();$i=[Console]::OpenStandardInput();$o=[Console]::OpenStandardOutput();Start-ThreadJob{$i.CopyTo($st)};$st.CopyTo($o)"
+//   - Setup:   Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0; Start-Service sshd
+//   - Config:  Match User tmgui -> ForceCommand powershell -Command "$s=New-Object System.Net.Sockets.TcpClient('127.0.0.1',9000);$st=$s.GetStream();$i=[Console]::OpenStandardInput();$o=[Console]::OpenStandardOutput();Start-ThreadJob{$i.CopyTo($st)};$st.CopyTo($o)"
 // Server Linux / macOS:
-//   - Setup:  sudo apt install openssh-server (Linux) / brew install openssh (macOS)
-//   - Config: Match User tmgui -> ForceCommand nc 127.0.0.1 9000
+//   - Setup:   sudo apt install openssh-server (Linux) / brew install openssh (macOS)
+//   - Config:  Match User tmgui -> ForceCommand nc 127.0.0.1 9000
 
 
 Console.WriteLine("TUI Terminal Server");
