1c4ac6461e84fe4602c7827cbf85dbda7b6ed325
friflo <ullrich@praetz.de>
2026-09-16 19:01:47 +0200

    TmGui - cleanup imports

--- src/Friflo.TmGui/GUI/Widget.Container/Window.cs
+++ src/Friflo.TmGui/GUI/Widget.Container/Window.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
-using Friflo.TmGui.TUI;
 
 
 // ReSharper disable SuggestVarOrType_BuiltInTypes
