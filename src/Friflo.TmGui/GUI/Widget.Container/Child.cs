00855f6e72c4996d8e81d0c6e310ac9874bfcd53
friflo <ullrich@praetz.de>
2026-09-18 19:56:20 +0200

    TmGui - separate Child.scrollArea.cs

--- src/Friflo.TmGui/GUI/Widget.Container/Child.cs
+++ src/Friflo.TmGui/GUI/Widget.Container/Child.cs
@@ -9,7 +9,6 @@ namespace Friflo.TmGui;
 
 public readonly ref partial struct GuiWidget
 {
-#region child
 	internal ChildScope BeginChild(WidgetID childId, Dim size)
 	{
 	    var window = Window;
@@ -45,54 +44,8 @@ public readonly ref partial struct GuiWidget
 	    window.SetCursor(scope.end.startCursor);
 	    MoveCursor(finalChildSize);
 	}
-#endregion
-
-
-#region scroll area
-	internal ScrollAreaScope BeginScrollArea(int childId, Dim size)
-	{
-	    var window		= Window;
-	    var startCursor = window.Cursor;
-	    window.PushScope(childId);
-
-	    // Compute outer bounds for the scroll area viewport
-	    var minHeight = 2 * LineHeight;
-	    var outerSize = window.WidgetSize(size, new Vector2(minHeight, minHeight));
-
-	    // Scroll areas ALWAYS require scissor clipping against their calculated outer size
-	    draw.PushScissor(startCursor, outerSize);
-	    var tui = draw.Tui;
-	    if (tui != null) {
-		    tui.FillRect(startCursor, outerSize, Colors.ScrollAreaColor);
-	    } else {
-		    draw.FillRect(startCursor, outerSize, Colors.ScrollAreaColor);
-	    }
-	    var scrollRect = PushScrollArea(childId, startCursor, outerSize, Sizes.ChildPadding);
-
-	    window.SetCursor(scrollRect.pos);
-	    PushLayout(LayoutDirection.Vertical, scrollRect.size);
-
-	    return new ScrollAreaScope(this, new ScrollAreaEnd(childId, startCursor, outerSize));
-	}
-
-	internal void EndScrollArea(in ScrollAreaScope scope)
-	{
-	    var window	= Window;
-	    var padding = Sizes.ChildPadding;
-	    
-		draw.PopScissor();
-
-	    // Measure base content including padding and focus outline clearance
-	    var rawContent = PopLayout();
-	    var scrollSize = rawContent + padding.Size;
-	    
-	    PopScrollArea(scope.end.childId, scope.end.startCursor, scope.end.outerSize, scrollSize, Colors.ScrollAreaColor, true);
-
-	    window.PopScope();
-	    
-	    window.SetCursor(scope.end.startCursor);
-	    MoveCursor(scope.end.outerSize);
-	}
-#endregion
 }
+
+
+// --------------------------------------------- Step-Rendering ---------------------------------------------
 
