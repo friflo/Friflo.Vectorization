9763a6d14908f5d499c8d3a1c20b68044d9614bb
friflo <ullrich@praetz.de>
2026-09-18 19:23:06 +0200

    TmGui - separate Style.cs

--- src/Friflo.TmGui/GUI/Gui.Widget.cs
+++ src/Friflo.TmGui/GUI/Gui.Widget.cs
@@ -99,29 +99,6 @@ public readonly ref partial struct GuiWidget
         currentStyle    = guiState.currentStyle;
     }
 
-
-#region Styles
-    internal StyleScope PushStyle(GuiStyle style)
-    {
-        var revertStyles = guiState.revertStyles;
-        var length       = revertStyles.Length;
-        if (guiState.revertStylesCount >= length) {
-            revertStyles = new RevertStyle[Math.Max(4, 2 * length)]; 
-            Array.Copy(guiState.revertStyles,  revertStyles, length);
-            guiState.revertStyles = revertStyles; 
-        }
-        ref var revertStyle = ref revertStyles[guiState.revertStylesCount++];
-        guiState.currentStyle.PushOverrides(style, ref revertStyle);
-        return new StyleScope(this);
-    }
-    
-    internal void PopStyle()
-    {
-        ref var revertStyle = ref guiState.revertStyles[--guiState.revertStylesCount];
-        guiState.currentStyle.PopOverrides(revertStyle);
-    }
-#endregion
-
     public void DrawFocus(Vector2 pos, Vector2 size)
     {
         var tui = draw.Tui;
---
+++ src/Friflo.TmGui/GUI/Widget/Style.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Friflo.TmGui;
+
+
+public readonly ref partial struct GuiWidget
+{
+    internal StyleScope PushStyle(GuiStyle style)
+    {
+        var revertStyles = guiState.revertStyles;
+        var length       = revertStyles.Length;
+        if (guiState.revertStylesCount >= length) {
+            revertStyles = new RevertStyle[Math.Max(4, 2 * length)]; 
+            Array.Copy(guiState.revertStyles,  revertStyles, length);
+            guiState.revertStyles = revertStyles; 
+        }
+        ref var revertStyle = ref revertStyles[guiState.revertStylesCount++];
+        guiState.currentStyle.PushOverrides(style, ref revertStyle);
+        return new StyleScope(this);
+    }
+    
+    internal void PopStyle()
+    {
+        ref var revertStyle = ref guiState.revertStyles[--guiState.revertStylesCount];
+        guiState.currentStyle.PopOverrides(revertStyle);
+    }
+}
+
+
+// --------------------------------------------- Step-Rendering --------------------------------------------- 
