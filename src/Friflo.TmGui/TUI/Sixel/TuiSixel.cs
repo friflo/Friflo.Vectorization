ca5b7a6b192c7aaeaebbaedabbaed6ed207a1e89
friflo <ullrich@praetz.de>
2026-09-19 12:22:46 +0200

    tui - add TuiSixel.UpdateFromRgb888()

--- src/Friflo.TmGui/TUI/Sixel/TuiSixel.cs
+++ src/Friflo.TmGui/TUI/Sixel/TuiSixel.cs
@@ -2,7 +2,9 @@
 // See LICENSE file in the project root for full license information.
 
 
+using System;
 
+// ReSharper disable SuggestVarOrType_BuiltInTypes
 // ReSharper disable once CheckNamespace
 namespace Friflo.TmGui.TUI;
 
@@ -11,6 +13,9 @@ public sealed class TuiSixel
     internal readonly   int     width;
     internal readonly   int     height;
     internal readonly   byte[]  data;
+    
+    // Size: exactly width * height bytes (1/4 of RGBA size)
+    internal readonly   byte[]  bitmaskData;
 
     public   override   string  ToString() => $"{width} x {height}";
 
@@ -18,5 +23,36 @@ public sealed class TuiSixel
         this.width  = width;
         this.height = height;
         this.data   = data;
+                
+        int bandCount = (height + 5) / 6;
+        
+        // Exact size: 1 byte per pixel (+ optional alignment padding if needed)
+        bitmaskData = new byte[width * height];
+        
+        UpdateFromRgb888(width, height, data, bandCount, bitmaskData, 4);
+    }
+
+    private static void UpdateFromRgb888(int width, int height, ReadOnlySpan<byte> src, int bandCount, byte[] bitmaskData, int bytesPerPixel)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            int rowOffset = y * width;
+            int srcRowOffset = rowOffset * bytesPerPixel;
+
+            for (int x = 0; x < width; x++)
+            {
+                int pixelOffset = srcRowOffset + (x * bytesPerPixel);
+
+                byte r = src[pixelOffset];
+                byte g = src[pixelOffset + 1];
+                byte b = src[pixelOffset + 2];
+
+                // Fast R3G3B2 color quantization (0..255)
+                byte colorIndex = (byte)((r & 0xE0) | ((g & 0xE0) >> 3) | (b >> 6));
+
+                // Direct 1-to-1 mapping into the width * height buffer
+                bitmaskData[rowOffset + x] = colorIndex;
+            }
+        }
     }
 }
