03fd81cf5c4606856fbde00acd503970c2cadfd8
friflo <ullrich@praetz.de>
2026-09-24 17:59:28 +0200

    tui - test:colored sixel text

--- src/Tests-TuiTerminal/TestGuiView.cs
+++ src/Tests-TuiTerminal/TestGuiView.cs
@@ -263,7 +263,7 @@ public partial class TestGuiView : IGuiView
 
         draw.FillCircle(new Vector2(280 + (int)x, 35), 25, 0xff88ffff);
         
-        draw.DrawText("ABC", new Vector2(10, 80), textColors);
+        draw.DrawText("sixel text", new Vector2(10, 70), textColors);
     }
     
 }
